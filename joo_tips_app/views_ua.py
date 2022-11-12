a0046d5a0c379c57d0cb344d27e1b52fff269dc0
Ivan Grigorev <iv.st.grigorev@gmail.com>
2022-11-12 14:30:13 +0100

    Modify views_ua.py

--- joo_tips_app/views_ua.py
+++ joo_tips_app/views_ua.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+import importlib
+import random
+from datetime import datetime, timedelta
 
 import ipinfo
 from django.contrib import messages
@@ -53,14 +55,185 @@ class ProgrammingLanguageChoiceUa(TemplateView):
         return render(request, template_name="ua/programming_language_choice_ua.html")
 
 
+# class PythonLessonUa(TemplateView):
+#     model = GuestsVisitStatistic
+#     template_name = "ua/web_site_in_process_ua.html"
+#
+#     def get(self, request, *args, **kwargs):
+#         record = self.model(language="Ukrainian", programming_language="Python")
+#         record.save()
+#         return render(request, template_name="ua/web_site_in_process_ua.html")
+
+
 class PythonLessonUa(TemplateView):
     model = GuestsVisitStatistic
-    template_name = "ua/web_site_in_process_ua.html"
+    template_name = "ua/python_ua/python_themes_time_ua.html"
+    test_time = ""
+    lesson_time = ""
+    guest_level = ""
 
     def get(self, request, *args, **kwargs):
         record = self.model(language="Ukrainian", programming_language="Python")
         record.save()
-        return render(request, template_name="ua/web_site_in_process_ua.html")
+        return render(request, template_name="ua/python_ua/python_themes_time_ua.html")
+
+    def post(self, request, *args, **kwargs):
+        self.test_time = (datetime.utcnow() + timedelta(minutes=int(request.POST.get("time")))).ctime()
+        self.lesson_time = (datetime.utcnow() + timedelta(minutes=int(request.POST.get("time")) / 2)).ctime()
+        self.guest_level = request.POST.get("level")
+        record = self.model(
+            guests_level=self.guest_level,
+            test_time=self.test_time,
+            lesson_time=self.lesson_time,
+            start_lesson_time=datetime.now(),
+        )
+        record.save()
+        return render(
+            request,
+            template_name="ua/python_ua/python_theory_ua.html",
+            context={"lesson_time": self.lesson_time, "timer": self.test_time},
+        )
+
+
+# class TheoryCardsUa TODO
+# @cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def python_theory_cards_ua(request):
+    global guests_card_1, guests_card_2, guests_card_3
+    guests_card_1 = random.randint(1, 10)
+    theme_1 = PythonBasicsTheory.objects.all().filter(id=guests_card_1)
+    guests_card_2 = random.randint(1, 4)
+    theme_2 = PythonVariablesTheory.objects.all().filter(id=guests_card_2)
+    guests_card_3 = random.randint(1, 4)
+    theme_3 = PythonDataTypesTheory.objects.all().filter(id=guests_card_3)
+    theme_4 = random.choice(
+        [
+            PythonBasicsTheory.objects.all().filter(id=random.randint(1, 10)),
+            PythonVariablesTheory.objects.all().filter(id=random.randint(1, 4)),
+            PythonDataTypesTheory.objects.all().filter(id=random.randint(1, 4)),
+        ]
+    )
+    text_ua = [theme_1[0], theme_2[0], theme_3[0], theme_4[0]]
+    if theme_4[0] == theme_1[0] or theme_4[0] == theme_2[0] or theme_4[0] == theme_3[0]:
+        theme_4 = random.choice(
+            [
+                PythonBasicsTheory.objects.all().filter(id=random.randint(1, 10)),
+                PythonVariablesTheory.objects.all().filter(id=random.randint(1, 4)),
+                PythonDataTypesTheory.objects.all().filter(id=random.randint(1, 4)),
+            ]
+        )
+        text_ua[3] = theme_4[0]
+    if request.method == "POST":
+        record = GuestsVisitStatistic(start_test_time=datetime.now())
+        record.save()
+        return redirect("python_theoretical_test_ua")
+    return render(request=request, template_name="ua/python_theory_ua.html", context={"text_ua": text_ua})
+
+
+theoretical_test_counter = 0
+practical_test_counter = 0
+right_answers = []
+tests_results = []
+
+
+# class TheoreticalTestUa TODO
+# @cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def python_theoretical_test_ua(request):
+    global question_theme_ua, theoretical_test_counter, right_answers, tests_results
+    question_theme_ua = random.choice(
+        [
+            PythonBasicsTheoreticalTask.objects.all().filter(card_id=guests_card_1),
+            PythonVariablesTheoreticalTask.objects.all().filter(card_id=guests_card_2),
+            PythonDataTypesTheoreticalTask.objects.all().filter(card_id=guests_card_3),
+        ]
+    )
+    right_answer = question_theme_ua.values_list("level_1_slot_1_right_answer_ua", flat=True)
+    right_answers.append(right_answer[0])
+    wrong_answer = question_theme_ua.values_list("level_1_slot_2_wrong_answer_ua", flat=True)
+    left_slot = random.choice([right_answer, wrong_answer])
+    right_slot = wrong_answer if left_slot == right_answer else right_answer
+    total_tests = 2
+    if request.method == "POST":
+        right_answers.append(right_answer[0])
+        tests_results.extend([1 if request.POST.get("slot") in right_answers else 0])
+        theoretical_test_counter += 1
+        if theoretical_test_counter == total_tests:
+            right_answers.append(right_answer[0])
+            theoretical_test_counter -= total_tests
+            record = GuestsVisitStatistic(
+                end_theoretical_start_practical_test_time=datetime.now(), theoretical_test_result=tests_results
+            )
+            record.save()
+            return redirect("python_practical_test_ua")
+        return redirect("python_theoretical_test_ua")
+    return render(
+        request,
+        template_name="ua/python_theoretical_test_ua.html",
+        context={
+            "test_counter": theoretical_test_counter + 1,
+            "total_tests": total_tests,
+            # 'timer': test_time,
+            "question_ua": question_theme_ua.values_list("question_ua", flat=True)[0],
+            "left_slot": left_slot,
+            "right_slot": right_slot,
+        },
+    )
+
+
+# class PracticalTestUa TODO
+# @cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def python_practical_test_ua(request):
+    global practical_test_counter, right_answers
+    folder = question_theme_ua.values_list("theme", flat=True)[0]
+    theme = folder
+    card = question_theme_ua.values_list("card_id", flat=True)[0]
+    test_number = random.choice([1, 2])
+    test_file = importlib.import_module(f"joo_tips_app.practical_tests.python.{folder}.{theme}_{card}_{test_number}")
+    total_tests = 6
+    answers = [test_file.var_r, test_file.var_w]
+    right_answers.append(test_file.var_r)
+    if request.method == "POST":
+        right_answers.append(test_file.var_r)
+        tests_results.extend([1 if request.POST.get("slot") in right_answers else 0])
+        practical_test_counter += 1
+        if practical_test_counter == total_tests:
+            practical_test_counter -= total_tests
+            record = GuestsVisitStatistic(practical_test_result=tests_results[2:], end_test_time=datetime.now())
+            record.save()
+            return redirect("progress_statistic_guests_ua")
+        return redirect("python_practical_test_ua")
+    return render(
+        request,
+        template_name="ua/python_practical_test_ua.html",
+        context={
+            "test_counter": practical_test_counter + 1,
+            "total_tests": total_tests,
+            # 'timer': test_time,
+            "question_ua": test_file.question_ua,
+            "code": test_file.var_u_screen,
+            "answers": answers,
+        },
+    )
+
+
+# class ProgressStatisticUa TODO
+def progress_statistic_guests_ua(request):
+    test_result = sum(tests_results)
+    day_result = 0
+    week_result = 0
+    month_result = 0
+    year_result = 0
+    tests_results.clear()
+    return render(
+        request,
+        template_name="ua/progress_statistic_ua.html",
+        context={
+            "test_result": test_result,
+            "day_result": day_result,
+            "week_result": week_result,
+            "month_result": month_result,
+            "year_result": year_result,
+        },
+    )
 
 
 class JavaScriptLessonUa(TemplateView):
