769316b5053ad2c452023fadbbc5d912d61fff75
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-06-28 18:29:58 +0300

    create a server folder

---
+++ src/server/db.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import "colors";
+dotenv.config();
+
+const URL = process.env.DB_URL;
+
+async function mongoDB() {
+  try {
+    await mongoose.connect(URL, {
+      dbName: "JOO_TIPS",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Database connection successful!".yellow);
+    return mongoose.connection;
+  } catch (error) {
+    console.error(`Error with MongoDB connection: ${error}`.red);
+    process.exit(1);
+  }
+}
+
+mongoDB()
+
+export default mongoDB;
