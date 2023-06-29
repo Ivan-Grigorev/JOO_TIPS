93a6ac7e50df1ac23c2c636225c2b98dd0f2fa1a
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-06-29 20:23:49 +0300

    add a check is user exist

--- src/server/middlewares/auth.js
+++ src/server/middlewares/auth.js
@@ -15,4 +15,20 @@ async function setIP(req, res, next) {
   }
 }
 
-module.exports = setIP;
+async function isUserExist(req, res, next) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user === null) {
+      return res.status(401).json({ error: "Email or password is wrong." });
+    }
+
+    req.user = user; //* store user in the request body
+  } catch (error) {
+    console.error(`${error}`.red);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+  next();
+}
+
+module.exports = { setIP, isUserExist };
