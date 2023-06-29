4dd55f20a2617e795423508ca39963574b7144fc
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-06-29 20:24:00 +0300

    add login feature

--- src/server/controllers/auth/auth.js
+++ src/server/controllers/auth/auth.js
@@ -67,6 +67,47 @@ async function register(req, res, next) {
   }
 }
 
+async function login(req, res, next) {
+  try {
+    const password = req.body.password; //* take a password from the request body
+    const userPassword = req.user.password; //* take a password from the user, stored on the past middleware
+    const userVerify = req.user.verify; //* take an verify status from the user
+
+    if (userVerify === false) {
+      req.status(400).json({ message: "Not verified." });
+    }
+    bcrypt.compare(password, userPassword, async (err, result) => {
+      if (err) return next(err);
+
+      if (result === false) {
+        return res.status(401).json({ error: "Email or password is wrong." });
+      }
+
+      const token = jwt.sign(
+        { id: req.user._id }, // hashed id
+        process.env.JWT_SECRET, // secret password
+        { expiresIn: "12h" } // life-time of the token
+      );
+
+      req.user.token = token;
+
+      await User.findByIdAndUpdate(req.user.id, req.user); // set the token
+
+      res.status(200).json({
+        token: req.user.token,
+        user: {
+          email: req.user.email,
+          subscription: req.user.subscription || "starter",
+        },
+      });
+    });
+  } catch (error) {
+    console.error(`Error while logging in: ${error}`.red);
+    return res.status(401).json({ message: error.message });
+  }
+}
+
 module.exports = {
   register,
+  login,
 };
--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -4,6 +4,8 @@ const middlewares = require("../middlewares/auth.js");
 
 const router = express.Router();
 
-router.post("/register",  auth.register);
+router.post("/register", auth.register);
+
+router.post("/login", middlewares.isUserExist, auth.login);
 
 module.exports = router;
