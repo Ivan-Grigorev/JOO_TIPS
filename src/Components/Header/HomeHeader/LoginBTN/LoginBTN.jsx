3d05bcc4ba8e08de46d2f126c74609debf9bf8c8
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-06-29 17:51:31 +0300

    moved to client folder

--- src/Components/Header/HomeHeader/LoginBTN/LoginBTN.jsx
+++
@@ -1,9 +0,0 @@
-const LoginBTN = ({isHidden}) => {
-  return (
-    <>
-      <button className={`btn-login ${isHidden}`}>LOG IN</button>
-    </>
-  );
-};
-
-export default LoginBTN;
--- src/Pages/InDev/InDev.jsx
+++
@@ -1,9 +0,0 @@
-const InDev = () => {
-  return (
-    <>
-      <>In still in develompent</>
-    </>
-  );
-};
-
-export default InDev;
