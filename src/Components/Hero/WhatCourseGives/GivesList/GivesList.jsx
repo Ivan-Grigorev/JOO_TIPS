a64c39e0e126a7ebc68656e6c3eeec4ab4a1db1c
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-06-30 21:45:45 +0300

    add arrows in 2 section

--- src/Components/Hero/WhatCourseGives/GivesList/GivesList.jsx
+++ src/Components/Hero/WhatCourseGives/GivesList/GivesList.jsx
@@ -1,7 +1,8 @@
 import "./GivesList.scss";
-import brain from "./images/brain 1.svg";
-import optimization from "./images/optimization 1.svg";
-import target from "./images/target 1.svg";
+import brain from "./icons/brain 1.svg";
+import optimization from "./icons/optimization 1.svg";
+import target from "./icons/target 1.svg";
+import arrow from "./icons/Arrow 6.svg";
 
 export default function GivesList() {
   return (
@@ -11,10 +12,20 @@ export default function GivesList() {
           <img src={brain} alt="brain icon" />
           <p>Розвиток здiбностей</p>
         </li>
+
+        <li className="arrow arrow-list-item-1">
+          <img src={arrow} alt="arrow icon" width="60px" />
+        </li>
+
         <li>
           <img src={optimization} alt="optimization icon" />
           <p>Корисне хобi</p>
         </li>
+
+        <li className="arrow arrow-list-item-2">
+          <img src={arrow} alt="arrow icon" width="60px" />
+        </li>
+
         <li>
           <img src={target} alt="target icon" />
           <p>Перспективна професiя</p>
