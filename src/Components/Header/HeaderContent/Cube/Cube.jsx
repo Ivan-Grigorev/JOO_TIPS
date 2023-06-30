b27ab156c12be9c9d33e35e8d7bab8e3a681f8a0
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-06-30 03:19:04 +0300

    add a dynamic Cube function

---
+++ src/Components/Header/HeaderContent/Cube/Cube.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import bigCube from "./bigCube.svg";
+import smallCube from "./smallCube.svg";
+
+import './Cube.css'
+
+export default function Cube() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const isLargeScreen = windowWidth >= 1440;
+
+  return (
+    <>
+      {!isLargeScreen ? (
+        <img
+          src={smallCube}
+          alt="a decoration small/medium cube"
+          className="mobile-cube"
+        />
+      ) : (
+        <img src={bigCube} alt="a decoration big cube" />
+      )}
+    </>
+  );
+}
