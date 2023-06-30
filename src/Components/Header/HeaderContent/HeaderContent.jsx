7ff5c997aa90a551a9ae04965a6735c05bebb656
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-06-30 03:19:11 +0300

    upd

--- src/Components/Header/HeaderContent/HeaderContent.jsx
+++ src/Components/Header/HeaderContent/HeaderContent.jsx
@@ -1,11 +1,15 @@
 import TryButton from "../../Button/TryButton/TryButton";
+import Cube from "./Cube/Cube";
 import Title from "./TextBlock/Title";
 
 export default function HeaderContent() {
   return (
     <>
       <Title />
+
       <TryButton text="Спробувати" />
+
+      <Cube />
     </>
   );
 }
