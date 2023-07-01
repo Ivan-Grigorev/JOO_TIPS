3c2d8c810ce3b80d7cd63f5d355eb4f0d9890046
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-01 12:06:45 +0300

    edit grids

--- src/Components/Hero/HeroTitle/Grids/Grids.jsx
+++ src/Components/Hero/HeroTitle/Grids/Grids.jsx
@@ -1,11 +1,12 @@
 import grid from "./grid.png";
-import './Grids.scss'
+import "./Grids.scss";
 
 export default function Grids() {
   return (
     <>
       <img src={grid} alt="decoration grid icon" className="grid grid-1" />
       <img src={grid} alt="decoration grid icon" className="grid grid-2" />
+      <img src={grid} alt="decoration grid icon" className="grid grid-3" />
     </>
   );
 }
