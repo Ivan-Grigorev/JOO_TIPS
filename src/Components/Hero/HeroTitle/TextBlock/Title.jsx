41d1fd651952f18544aeaa8fa8312799810bb1b8
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-03 02:08:25 +0300

    add sparkles in hero title

--- src/Components/Hero/HeroTitle/TextBlock/Title.jsx
+++ src/Components/Hero/HeroTitle/TextBlock/Title.jsx
@@ -1,14 +1,14 @@
 import "./Title.scss";
-
+import sparkles from "./sparkles.svg";
 import { memo } from "react";
 
 const Title = memo(() => {
-
   return (
     <>
       <div className="textBlock">
         <h1>
           <span>IT</span> - ПРОФЕСІЯ
+          <img src={sparkles} alt="sparkles" className="hero__sparkles" />
         </h1>
         <h2>для школярiв та дорослих </h2>
         <h3>
