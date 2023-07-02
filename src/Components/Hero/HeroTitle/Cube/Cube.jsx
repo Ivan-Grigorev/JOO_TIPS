f966d704ab10ad94bcb04a7ff2f532237e271fe7
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-03 01:57:30 +0300

    upd cubes, optimized it

--- src/Components/Hero/HeroTitle/Cube/Cube.jsx
+++ src/Components/Hero/HeroTitle/Cube/Cube.jsx
@@ -6,40 +6,20 @@ import "./Cube.scss";
 import { memo } from "react";
 
 const Cube = () => {
-  // console.log("Im render again (Cube)");
   // TODO обязательно поставить медиаправила, вместо этого стейта
-
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const isLargeScreen = windowWidth >= 1320;
 
   return (
     <>
-      {!isLargeScreen ? (
-        <img
-          src={smallCube}
-          alt="a decoration small/medium cube"
-          className="cube mobile-cube"
-        />
-      ) : (
-        <img
-          src={bigCube}
-          alt="a decoration big cube"
-          className="cube large-cube"
-        />
-      )}
+      <img
+        src={smallCube}
+        alt="a decoration small/medium cube"
+        className="cube mobile-cube"
+      />
+      <img
+        src={bigCube}
+        alt="a decoration big cube"
+        className="cube large-cube"
+      />
     </>
   );
 };
