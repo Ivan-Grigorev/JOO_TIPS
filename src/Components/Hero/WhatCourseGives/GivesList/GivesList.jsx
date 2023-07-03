cd6f89387f12b3422499db6c2f121bb9f6a3f92b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-03 16:18:27 +0300

    add lazy load for images

--- src/Components/Hero/WhatCourseGives/GivesList/GivesList.jsx
+++ src/Components/Hero/WhatCourseGives/GivesList/GivesList.jsx
@@ -11,25 +11,25 @@ const GivesList = () => {
     <>
       <ul>
         <li>
-          <img src={brain} alt="brain icon" />
+          <img src={brain} alt="brain icon" loading="lazy" />
           <p>Розвиток здiбностей</p>
         </li>
 
         <li className="arrow arrow-list-item-1">
-          <img src={arrow} alt="arrow icon" width="60px" />
+          <img src={arrow} alt="arrow icon" width="60px" loading="lazy" />
         </li>
 
         <li>
-          <img src={optimization} alt="optimization icon" />
+          <img src={optimization} alt="optimization icon" loading="lazy" />
           <p>Корисне хобi</p>
         </li>
 
         <li className="arrow arrow-list-item-2">
-          <img src={arrow} alt="arrow icon" width="60px" />
+          <img src={arrow} alt="arrow icon" width="60px" loading="lazy" />
         </li>
 
         <li>
-          <img src={target} alt="target icon" />
+          <img src={target} alt="target icon" loading="lazy" />
           <p>Перспективна професiя</p>
         </li>
       </ul>
