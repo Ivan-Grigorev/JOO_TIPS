5ec667beb85dfd0a67accc493cc081f8a29c710b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-04 22:12:32 +0300

    upd user-joi passwords models

--- src/server/models/user/user-joi.js
+++ src/server/models/user/user-joi.js
@@ -3,11 +3,11 @@ require("colors");
 
 function userJoi(req, res, next) {
   const userSchema = Joi.object({
-    name: Joi.string().min(3),
+    name: Joi.string().min(4).max(20),
     email: Joi.string().email(),
     avatar: Joi.string().min(5),
-    password: Joi.string().min(3),
-    confirmedPassword: Joi.string().min(3),
+    password: Joi.string(),
+    confirmedPassword: Joi.string(),
     token: Joi.string(),
     subscription: Joi.string(),
   });
