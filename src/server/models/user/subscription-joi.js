dcdb50882f60f4e4e225c452c81bd66e29a0598d
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-05 17:37:27 +0300

    create a subscription-joi.js

---
+++ src/server/models/user/subscription-joi.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+require("colors");
+
+function subscriptionJoi(req, res, next) {
+  const subscriptionSchema = Joi.object({
+    subscription: {
+      type: Joi.string(),
+      isPremium: Joi.boolean(),
+      expiredIn: Joi.number(),
+    },
+  });
+
+  const { error } = subscriptionSchema.validate(req.body);
+
+  if (error) {
+    console.error(`Joi subscription error: ${error}`.red);
+    return res.status(400).json({ message: error.message }).end();
+  }
+
+  next();
+}
+
+module.exports = subscriptionJoi;
