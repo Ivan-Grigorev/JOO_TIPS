d60205000cb1d7a50844ddd15a5cdb52b72e994e
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-09 17:03:43 +0300

    upd. settings inputs, edit focus and hover styles. Edit header styles

--- src/Components/Profile/Settings/Fields/Fields.jsx
+++ src/Components/Profile/Settings/Fields/Fields.jsx
@@ -3,30 +3,75 @@ import { Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
 const Fields = () => {
-  const [value, setValue] = useState("");
-  const handleChange = (event) => setValue(event.target.value);
+  // Define the state for each input field
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [avatarName, setAvatarName] = useState("");
+  const [about, setAbout] = useState("");
+  const [language, setLanguage] = useState("");
+  const [notifications, setNotifications] = useState("");
+
+  // Define a single onChange handler
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    // Update the correct state based on the input field
+    switch (name) {
+      case "number":
+        setNumber(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "avatarName":
+        setAvatarName(value);
+        break;
+      case "about":
+        setAbout(value);
+        break;
+      case "language":
+        setLanguage(value);
+        break;
+      case "notifications":
+        setNotifications(value);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div className="fields">
       <Text fontWeight="700">+380 (50) 111 11 11</Text>
       <Input
-        value={value}
+        value={number} // Use the corresponding state
         name="number"
-        onChange={handleChange}
+        onChange={handleChange} // Use the shared handler
         placeholder="Tap to change number"
+        autoComplete="off"
         size="sm"
         borderTop="none"
         borderLeft="none"
         borderRight="none"
         borderBottom="1px solid"
         borderColor="gray.500"
+        _hover={{
+          borderBottomColor: "blue.500",
+        }}
+        _focus={{
+          borderBottomColor: "blue.500",
+        }}
+        _focusVisible={{
+          borderBottomColor: "blue.500",
+        }}
       />
 
+      {/* Similar changes for the other input fields... */}
       <Text fontWeight="700">joker@gmail.com</Text>
       <Input
-        value={value}
+        value={email} // Use the corresponding state
         name="email"
-        onChange={handleChange}
+        onChange={handleChange} // Use the shared handler
         placeholder="Tap to change e-mail"
         size="sm"
         borderTop="none"
@@ -34,12 +79,22 @@ const Fields = () => {
         borderRight="none"
         borderBottom="1px solid"
         borderColor="gray.500"
+        _hover={{
+          borderBottomColor: "blue.500",
+        }}
+        _focus={{
+          borderBottomColor: "blue.500",
+        }}
+        _focusVisible={{
+          borderBottomColor: "blue.500",
+        }}
       />
 
       <Text fontWeight="700">@Frog_frog</Text>
       <Input
-        value={value}
+        value={avatarName}
         name="avatarName"
+        autoComplete="off"
         onChange={handleChange}
         placeholder="Tap to change avatar name"
         size="sm"
@@ -48,12 +103,22 @@ const Fields = () => {
         borderRight="none"
         borderBottom="1px solid"
         borderColor="gray.500"
+        _hover={{
+          borderBottomColor: "blue.500",
+        }}
+        _focus={{
+          borderBottomColor: "blue.500",
+        }}
+        _focusVisible={{
+          borderBottomColor: "blue.500",
+        }}
       />
 
       <Text fontWeight="700">I'll be a genius programmer</Text>
       <Input
-        value={value}
+        value={about}
         name="about"
+        autoComplete="off"
         onChange={handleChange}
         placeholder="Tap to change about section"
         size="sm"
@@ -62,12 +127,22 @@ const Fields = () => {
         borderRight="none"
         borderBottom="1px solid"
         borderColor="gray.500"
+        _hover={{
+          borderBottomColor: "blue.500",
+        }}
+        _focus={{
+          borderBottomColor: "blue.500",
+        }}
+        _focusVisible={{
+          borderBottomColor: "blue.500",
+        }}
       />
 
       <Text fontWeight="700">English</Text>
       <Input
-        value={value}
+        value={language}
         name="language"
+        autoComplete="off"
         onChange={handleChange}
         placeholder="Tap to change language"
         size="sm"
@@ -76,11 +151,21 @@ const Fields = () => {
         borderRight="none"
         borderBottom="1px solid"
         borderColor="gray.500"
+        _hover={{
+          borderBottomColor: "blue.500",
+        }}
+        _focus={{
+          borderBottomColor: "blue.500",
+        }}
+        _focusVisible={{
+          borderBottomColor: "blue.500",
+        }}
       />
       <Text fontWeight="700">Sound and vibration</Text>
       <Input
-        value={value}
+        value={notifications}
         name="notifications"
+        autoComplete="off"
         onChange={handleChange}
         placeholder="Tap to change setting"
         size="sm"
@@ -89,6 +174,15 @@ const Fields = () => {
         borderRight="none"
         borderBottom="1px solid"
         borderColor="gray.500"
+        _hover={{
+          borderBottomColor: "blue.500",
+        }}
+        _focus={{
+          borderBottomColor: "blue.500",
+        }}
+        _focusVisible={{
+          borderBottomColor: "blue.500",
+        }}
       />
     </div>
   );
