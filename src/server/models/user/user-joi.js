937e8c19ade0b4d8f4368fe74b7ddb2c9bb07d94
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-12 00:42:42 +0300

    Edit max length error

--- src/server/models/user/user-joi.js
+++ src/server/models/user/user-joi.js
@@ -58,8 +58,9 @@ function userDeleteJoi(req, res, next) {
 
 function userUpdateProfile(req, res, next) {
   const userDeleteSchema = Joi.object({
-    phone: Joi.string().min(5).allow(null).messages({
+    phone: Joi.string().min(5).max(10).allow(null).messages({
       "string.min": `Phone number should have a minimum length of {#limit}`,
+      "string.max": `Phone number should have a maximum length of {#limit}`,
     }),
     email: Joi.string().email().allow(null).messages({
       "string.email": "Email must be a valid email",
