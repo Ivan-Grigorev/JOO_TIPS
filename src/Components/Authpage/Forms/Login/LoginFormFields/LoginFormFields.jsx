4ccdd83a197124679441bd42d46a451959e6c786
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-12 19:47:46 +0300

    create an EmailSVG.jsx for reusage

--- src/Components/Authpage/Forms/Login/LoginFormFields/LoginFormFields.jsx
+++ src/Components/Authpage/Forms/Login/LoginFormFields/LoginFormFields.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import loginAt from "../.././icons/login_at.svg";
 import loginLockOpen from "../.././icons/login_lock_open.svg";
+import EmailSVG from "../../icons/EmailSVG";
 
 export default function LoginFormFields({
   handleChange,
@@ -13,7 +13,7 @@ export default function LoginFormFields({
     const messages = document.querySelector("#login-form .message");
     const show = () => (messages.style.opacity = 1);
     const hide = () => (messages.style.opacity = 0);
-    
+
     // it's get it more smoothly
     if (errors.length > 0) inputs.some((input) => input.value.length > 0) ? show() : hide(); // prettier-ignore
   });
@@ -21,7 +21,7 @@ export default function LoginFormFields({
   return (
     <>
       <label className="form-field name" htmlFor="email">
-        <img className="auth-icons user-image" src={loginAt} alt="mail icon" />
+        <EmailSVG />
         <input
           name="email"
           placeholder="Email"
---
+++ src/Components/Authpage/Forms/icons/EmailSVG.jsx
@@ -0,0 +1,7 @@
+import email from "./login_at.svg";
+
+const EmailSVG = () => {
+  return <img className="auth-icons user-image" src={email} alt="mail icon" />;
+};
+
+export default EmailSVG;
