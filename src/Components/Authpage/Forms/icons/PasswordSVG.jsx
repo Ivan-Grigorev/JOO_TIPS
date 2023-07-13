35cdb635d7c3f4724ef018ebbc86fab8f968a7cb
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-13 23:40:26 +0300

    create svg-components

---
+++ src/Components/Authpage/Forms/icons/PasswordSVG.jsx
@@ -0,0 +1,12 @@
+import password from "../icons/login_lock_open.svg";
+
+const PasswordSVG = () => {
+  return (
+    <>
+      <img src={password} alt="password" className="auth-icons lock-image" />
+      <></>
+    </>
+  );
+};
+
+export default PasswordSVG;
---
+++ src/Components/Authpage/Forms/icons/UserSVG.jsx
@@ -0,0 +1,11 @@
+import user from "./login_user.svg";
+
+const UserSVG = () => {
+  return (
+    <>
+      <img src={user} alt="user" className="auth-icons user-image" />
+    </>
+  );
+};
+
+export default UserSVG;
