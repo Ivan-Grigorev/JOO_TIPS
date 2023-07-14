30542261fe512357f36d8b7de7f88ac1ee12cb95
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-15 01:15:22 +0300

    add back-link to profile/settings (with styles)

--- src/Components/Profile/Settings/BackButton/BackButton.jsx
+++ src/Components/Profile/Settings/BackButton/BackButton.jsx
@@ -2,13 +2,13 @@ import { GoArrowLeft } from "react-icons/go";
 import "./BackButton.scss";
 import { Link } from "react-router-dom";
 
-const BackButton = ({ currentSectionName }) => {
+const BackButton = ({ backLink, currentSectionName }) => {
   return (
     <div className="back-button">
-      <Link to={null}>
+      <Link to={backLink}>
         <GoArrowLeft />
+        <p className="profile-title">{currentSectionName}</p>
       </Link>
-      <p className="profile-title">{currentSectionName}</p>
     </div>
   );
 };
--- src/Components/Profile/Settings/Settings.jsx
+++ src/Components/Profile/Settings/Settings.jsx
@@ -2,11 +2,14 @@ import "./Settings.scss";
 import BackButton from "./BackButton/BackButton";
 import UpdateAvatar from "./UpdateAvatar/UpdateAvatar";
 import Form from "./Fields/Form";
+import { useLocation } from "react-router-dom";
 
 const Settings = () => {
+  const backLink = useLocation().state?.from ?? "/";
+
   return (
     <>
-      <BackButton currentSectionName="Settings" />
+      <BackButton currentSectionName="Settings" backLink={backLink} />
 
       <UpdateAvatar />
 
