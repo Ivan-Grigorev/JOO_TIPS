0e2847a220a39357452cd583668c9d52302ad845
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-14 10:09:25 +0300

    add location hook to watching for hash. From hash depends a card which user would see(login or signup)

--- src/Components/Authpage/Switcher/Switcher.jsx
+++ src/Components/Authpage/Switcher/Switcher.jsx
@@ -1,11 +1,34 @@
-export default function Switcher() {
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const Switcher = () => {
+  const location = useLocation();
+  const [isChecked, setIsChecked] = useState(false);
+
+  useEffect(() => {
+    setIsChecked(location.hash === "#login");
+  }, [location]);
+
+  const toggleCheck = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <>
       <h1>
         <span>Sign up</span>Log in
       </h1>
-      <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" />
+      <input
+        className="checkbox"
+        type="checkbox"
+        id="reg-log"
+        name="reg-log"
+        checked={isChecked}
+        onChange={toggleCheck}
+      />
       <label htmlFor="reg-log"></label>
     </>
   );
-}
+};
+
+export default Switcher;
