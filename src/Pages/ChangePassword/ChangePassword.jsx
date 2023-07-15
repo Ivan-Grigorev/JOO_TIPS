e164e34e7d1162fa443cff2d3d1d976ee9df42dc
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-15 04:07:35 +0300

    add comments

--- src/Pages/ChangePassword/ChangePassword.jsx
+++ src/Pages/ChangePassword/ChangePassword.jsx
@@ -1,3 +1,4 @@
+// Import necessary libraries, components, hooks and actions
 import { useState } from "react";
 import ChangePasswordForm from "../../Components/ChangePassword/ChangePasswordForm";
 import LogoLink from "../../Components/Header/HomeHeader/Navigation/Links/LogoLink";
@@ -14,19 +15,25 @@ import {
 import PasswordWasChanged from "../../Components/ChangePassword/PasswordWasChanged";
 import { unwrapResult } from "@reduxjs/toolkit";
 
+// Main component
 const ChangePassword = () => {
+  // Create useDispatch hook instance
   const dispatch = useDispatch();
 
+  // Use useState to handle password states
   const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmedNewPassword, setConfirmedNewPassword] = useState("");
   const [isPasswordChanged, setIsPasswordChanged] = useState(false);
 
+  // Use useSelector to select and handle errors related to password from Redux store
   const errors = useSelector(selectUserErrors).password;
 
+  // Function to handle input field changes
   const handleChange = (event) => {
     const { name, value } = event.target;
 
+    // Use the 'name' attribute to determine which state update function to call
     switch (name) {
       case "currentPassword":
         setCurrentPassword(value);
@@ -42,12 +49,15 @@ const ChangePassword = () => {
     }
   };
 
+  // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Check if newPassword and confirmedNewPassword match
     const match = newPassword === confirmedNewPassword;
     if (!match) {
       dispatch(resetPasswordErrors());
+      // Dispatch an action to set an error message in case the passwords do not match
       return dispatch(
         handleSetError({
           field: "password",
@@ -56,13 +66,15 @@ const ChangePassword = () => {
       );
     }
 
+    // Attempt to change the password and handle the result
     try {
       const action = await dispatch(changePassword({ currentPassword, password: newPassword })); // prettier-ignore
-      const originalPromiseResult = unwrapResult(action); //! do not delete!!!
-      // Если запрос успешен, то переход в след. стадию
+      const originalPromiseResult = unwrapResult(action); // If the promise is resolved, this line will return the fulfilled value. If rejected, it will throw the error.
+      // If the password change is successful, set isPasswordChanged to true
       setIsPasswordChanged(true);
     } catch (rejectedValueOrSerializedError) {
-      console.log(rejectedValueOrSerializedError); // Если запрос не успешен, выведет ошибку
+      // Log the error if the password change failed
+      console.log(rejectedValueOrSerializedError); 
     }
   };
 
@@ -74,6 +86,7 @@ const ChangePassword = () => {
         </header>
 
         <main>
+          {/* If the password has been changed, show the PasswordWasChanged component, otherwise, show the ChangePasswordForm component */}
           {!isPasswordChanged ? (
             <ChangePasswordForm
               currentPassword={currentPassword}
