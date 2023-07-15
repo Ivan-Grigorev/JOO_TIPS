09058d8070efcadb99904b3d12f486eb90a7fff5
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-15 04:40:06 +0300

    add useEffect

--- src/Components/RecoveringPage/PasswordHasBeenChanged/PasswordHasBeenChanged.jsx
+++ src/Components/RecoveringPage/PasswordHasBeenChanged/PasswordHasBeenChanged.jsx
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router-dom";
-import '../../../Pages/RecoveringPassword/RecoveringPassword.scss'
+import "../../../Pages/RecoveringPassword/RecoveringPassword.scss";
+import { useEffect } from "react";
 
 // This component is used to display a message to the user after they have successfully changed their password
 const PasswordHasBeenChanged = () => {
   // The useNavigate hook provides easy navigation between the routes in a React Router application
   const navigate = useNavigate();
 
-  // Using setTimeout to navigate the user to the signup page after 5 seconds
-  setTimeout(() => navigate("/signup#login"), 5000);
+  useEffect(() => {
+    // Using setTimeout to navigate the user to the signup page after 5 seconds
+    setTimeout(() => navigate("/signup#login"), 5000);
+  });
 
   return (
     <>
