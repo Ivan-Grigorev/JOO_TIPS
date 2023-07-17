a67da559b4b3048ad34a413788830e5cbfe11a18
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-17 04:23:49 +0300

    fix bug

--- src/server/controllers/referral/referral.js
+++ src/server/controllers/referral/referral.js
@@ -1,10 +1,12 @@
 const User = require("../../models/user/user");
 
-async function increaseReferralCount(req, res, next) {
+async function increaseReferralCount(req, res) {
   try {
     const ID = req.user.id;
 
     await User.findByIdAndUpdate(ID, { $inc: { "referral.count": 1 } });
+
+    res.status(204).end();
   } catch (error) {
     console.log(`Error while increase referral count: ${error.message}`.red);
     res
