54f99662bf0231e815c0b4b733cb16885452dd86
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-21 01:57:24 +0300

    upd

--- src/Components/Cookie/Cookie.jsx
+++ src/Components/Cookie/Cookie.jsx
@@ -1,16 +1,58 @@
-import { Checkbox } from "@chakra-ui/react";
+import { Checkbox, Button, ButtonGroup } from "@chakra-ui/react";
+import { useState } from "react";
 import "./Cookie.scss";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import { setCookies } from "../../redux/cookies/cookies-operations";
+
 const CookieBanner = () => {
-  // const handleDeclineAll = () => {
-  //   console.log("decline all");
-  // };
-  // const handleAcceptNeccessary = () => {
-  //   console.log("user only neccessary");
-  // };
-  // const handleAcceptAll = () => {
-  //   console.log("Accept all");
-  // };
+  const [selectedCookies, setSelectedCookies] = useState({
+    strictlyNecessary: true,
+    performance: true,
+    targeting: true,
+    functionality: true,
+  });
+  const [buttonText, setButtonText] = useState("Accept all"); // новый state для текста кнопки
+  const dispatch = useDispatch();
+
+  const setCookieValue = (type, value) => {
+    setSelectedCookies((prevState) => ({
+      ...prevState,
+      [type]: value,
+    }));
+
+    // изменяем текст кнопки
+    setButtonText("Save & Close");
+
+    // сохраняем выбор пользователя
+    // dispatch(setCookies({ [type]: value }));
+  };
+
+  const handleAcceptAll = () => {
+    setButtonText("Save & Close"); // изменяем текст кнопки
+    dispatch(setCookies(selectedCookies));
+
+    // todo ЗАКРЫТЬ БАННЕР
+  };
+
+  const handleDeclineAll = () => {
+    setSelectedCookies({
+      strictlyNecessary: true,
+      performance: false,
+      targeting: false,
+      functionality: false,
+    });
+    setButtonText("Save & Close"); // изменяем текст кнопки
+    dispatch(
+      setCookies({
+        strictlyNecessary: true,
+        performance: false,
+        targeting: false,
+        functionality: false,
+      })
+    );
+
+    // todo ЗАКРЫТЬ БАННЕР
+  };
 
   return (
     <>
@@ -22,28 +64,47 @@ const CookieBanner = () => {
           Privacy policy.
         </p>
 
-        <ul classname="cookie-list">
+        <ul className="cookie-list">
           <li>
             <Checkbox isDisabled defaultChecked size="sm">
               Strictly neccessary
             </Checkbox>
           </li>
           <li>
-            <Checkbox size="sm">Perfomance</Checkbox>
+            <Checkbox
+              size="sm"
+              isChecked={selectedCookies.performance}
+              onChange={(e) => setCookieValue("performance", e.target.checked)}
+            >
+              Performance
+            </Checkbox>
           </li>
           <li>
-            <Checkbox size="sm">Targeting</Checkbox>
+            <Checkbox
+              size="sm"
+              isChecked={selectedCookies.targeting}
+              onChange={(e) => setCookieValue("targeting", e.target.checked)}
+            >
+              Targeting
+            </Checkbox>
           </li>
           <li>
-            <Checkbox size="sm">Functionality</Checkbox>
+            <Checkbox
+              size="sm"
+              isChecked={selectedCookies.functionality}
+              onChange={(e) =>
+                setCookieValue("functionality", e.target.checked)
+              }
+            >
+              Functionality
+            </Checkbox>
           </li>
         </ul>
-
         <ButtonGroup>
-          <Button size="xs" colorScheme="green">
-            Accept all
+          <Button size="xs" colorScheme="green" onClick={handleAcceptAll}>
+            {buttonText} {/* используем переменную для управления текстом */}
           </Button>
-          <Button size="xs" colorScheme="red">
+          <Button size="xs" colorScheme="red" onClick={handleDeclineAll}>
             Decline all
           </Button>
         </ButtonGroup>
