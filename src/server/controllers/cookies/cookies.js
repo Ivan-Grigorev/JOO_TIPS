b5975345bada0ea10920a8ce3a774de2533b0397
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-22 14:56:02 +0300

    upd, fix bugs

--- src/server/controllers/cookies/cookies.js
+++ src/server/controllers/cookies/cookies.js
@@ -1,22 +1,32 @@
+const { encrypt } = require("../../utils/encrypt");
+const { obfuscate } = require("../../utils/obfuscate");
+
 async function set(req, res, next) {
   try {
-    const cookies = req.body; // Предполагаем, что тело запроса содержит cookies
-    const setCookies = {}; // Создаём объект для сохранения установленных кук
+    const cookies = req.body;
+    const setCookies = {};
 
     for (let cookieName in cookies) {
-      // Устанавливаем каждый cookie из тела запроса
-      const cookieValue = cookies[cookieName];
-      res.cookie(cookieName, cookieValue, {
+      const rawCookieValue = cookies[cookieName];
+
+      // Шифруем и обфусцируем значение cookie
+      const encryptedValue = await encrypt(String(rawCookieValue));
+      const obfuscatedName = obfuscate(cookieName);
+      const obfuscatedValue = obfuscate(encryptedValue);
+
+      // Устанавливаем cookie с обфусцированным именем и значением
+      res.cookie(obfuscatedName, obfuscatedValue, {
         httpOnly: true,
         secure: true,
         sameSite: "strict",
       });
-      setCookies[cookieName] = cookieValue; // Добавляем установленные куки в объект
+
+      setCookies[obfuscatedName] = obfuscatedValue;
     }
 
     res.status(200).send({ message: "Cookies set successfully" });
   } catch (error) {
-    console.error("Error setting cookies".red);
+    console.error("Error setting cookies");
     res.status(500).json({ message: "Internal server error" });
   }
 }
