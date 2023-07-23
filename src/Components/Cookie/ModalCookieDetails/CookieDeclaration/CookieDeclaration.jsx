84293beee87e65154b5e76c9faa1777f34052b58
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-24 01:30:40 +0300

    Add more items, add scroll

--- src/Components/Cookie/ModalCookieDetails/CookieDeclaration/CookieDeclaration.jsx
+++ src/Components/Cookie/ModalCookieDetails/CookieDeclaration/CookieDeclaration.jsx
@@ -1,29 +1,61 @@
-import {
-  Checkbox,
-  FormControl,
-  FormLabel,
-  SimpleGrid,
-  Switch,
-} from "@chakra-ui/react";
+import { FormControl, FormLabel, SimpleGrid, Switch } from "@chakra-ui/react";
 import "./CookieDeclaration.scss";
 
 const CookieDeclaration = ({ selectedCookies, setCookieValue }) => {
   return (
     <>
-      <FormControl as={SimpleGrid} columns={{ base: 2, lg: 4 }}>
-        <FormLabel htmlFor="isChecked">
-          <span className="cookie-name">
-            Strictly Necessary <br />
-          </span>
-          <p className="cookie-description">
-            Strictly necessary cookies are essential for the proper functioning
-            of a website. They enable basic functionalities like page
-            navigation, access to secure areas, and form submissions. These
-            cookies do not require user consent, as the website cannot function
-            correctly without them.
-          </p>
-        </FormLabel>
-        <Switch id="isChecked" isChecked isDisabled />
+      <FormControl
+        as={SimpleGrid}
+        columns={{ base: 2, lg: 4 }}
+        className="cookie-list"
+      >
+        <div className="cookie-list__item">
+          <FormLabel htmlFor="neccessary">
+            <span className="cookie-name">
+              Strictly Necessary <br />
+            </span>
+            <p className="cookie-description">
+              Strictly necessary cookies are essential for the proper
+              functioning of a website. They enable basic functionalities like
+              page navigation, access to secure areas, and form submissions.
+              These cookies do not require user consent, as the website cannot
+              function correctly without them.
+            </p>
+          </FormLabel>
+          <Switch id="neccessary" isChecked isDisabled />
+        </div>
+
+        <div className="cookie-list__item">
+          <FormLabel htmlFor="neccessary">
+            <span className="cookie-name">
+              Strictly Necessary <br />
+            </span>
+            <p className="cookie-description">
+              Strictly necessary cookies are essential for the proper
+              functioning of a website. They enable basic functionalities like
+              page navigation, access to secure areas, and form submissions.
+              These cookies do not require user consent, as the website cannot
+              function correctly without them.
+            </p>
+          </FormLabel>
+          <Switch id="neccessary" isChecked isDisabled />
+        </div>
+
+        <div className="cookie-list__item">
+          <FormLabel htmlFor="neccessary">
+            <span className="cookie-name">
+              Strictly Necessary <br />
+            </span>
+            <p className="cookie-description">
+              Strictly necessary cookies are essential for the proper
+              functioning of a website. They enable basic functionalities like
+              page navigation, access to secure areas, and form submissions.
+              These cookies do not require user consent, as the website cannot
+              function correctly without them.
+            </p>
+          </FormLabel>
+          <Switch id="neccessary" isChecked isDisabled />
+        </div>
       </FormControl>
     </>
   );
