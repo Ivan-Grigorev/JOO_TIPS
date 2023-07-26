1114e4953655a5c9939b1ec469885d71416ab21e
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-26 16:42:22 +0300

    update from class to className

--- src/Components/Cookie/ModalCookieDetails/CookieAbout/CookieAbout.jsx
+++ src/Components/Cookie/ModalCookieDetails/CookieAbout/CookieAbout.jsx
@@ -4,7 +4,7 @@ const CookieAbout = () => {
       <p>
         Cookies are small text files that websites store on your computer or
         device when you visit them.{" "}
-        <span class="bold">
+        <span className="bold">
           These files contain a small amount of data, such as information about
           your visit to the site, preferences, or authentication details.
         </span>
@@ -14,7 +14,7 @@ const CookieAbout = () => {
         The main purpose of cookies is to help websites remember specific
         information about you to provide a more personalized and convenient user
         experience.{" "}
-        <span class="bold">
+        <span className="bold">
           For example, when you log in to a site using your credentials, cookies
           allow you to remain authenticated throughout your session, so you
           don't have to enter your login and password every time.
@@ -22,7 +22,7 @@ const CookieAbout = () => {
       </p>
 
       <p>
-        <span class="bold">
+        <span className="bold">
           Cookies can be either temporary (session cookies), which are stored
           only for the duration of your visit to the site, or persistent, which
           are stored on your device for a longer period.
@@ -33,7 +33,7 @@ const CookieAbout = () => {
 
       <p>
         It's essential to note that{" "}
-        <span class="bold">
+        <span className="bold">
           the storage of cookies may be limited by your browser settings, and
           you can choose whether you want to allow sites to store them on your
           device or prefer to delete them after each visit.
@@ -43,7 +43,7 @@ const CookieAbout = () => {
       </p>
 
       <p>
-        <span class="bold">
+        <span className="bold">
           Furthermore, as a user, you have control over your cookie preferences,
           and you can always modify these settings in your personal account or
           profile on the website.
