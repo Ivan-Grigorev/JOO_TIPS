6c60154efe34b320bb0a3a4fdbe92039be29cace
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-26 18:46:46 +0300

    upd morgan.js, edit output folder

--- src/server/security/morgan.js
+++ src/server/security/morgan.js
@@ -2,12 +2,16 @@ const fs = require("fs");
 const path = require("path");
 const morgan = require("morgan");
 
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "access.log"),
-  { flags: "a" }
-);
-
 const morganLogs = (app) => {
+  if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+    fs.mkdirSync(path.join(__dirname, "..", "logs"));
+  }
+
+  const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "..", "logs", "access.log"), // Здесь мы идем на один уровень выше, а затем в папку 'logs'
+    { flags: "a" }
+  );
+  
   app.use(morgan("combined", { stream: accessLogStream }));
 };
 
