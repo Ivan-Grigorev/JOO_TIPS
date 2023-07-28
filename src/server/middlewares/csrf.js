bc7357f79b2e6005e0647e6d0e57bd728ed745a3
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-28 22:52:43 +0300

    upd

--- src/server/middlewares/csrf.js
+++ src/server/middlewares/csrf.js
@@ -1,11 +1,11 @@
 const checkCSRF = (req, res, next) => {
   if (!req.session) {
     console.log("Session expired, please log in again");
-    res.status(401).send("Session expired, please log in again");
+    res.status(401).json({ message: "Session expired, please log in again" });
   } else {
     // console.log("csrf is exist");
     next();
   }
 };
 
-module.exports = checkCSRF;
+module.exports = { checkCSRF };
