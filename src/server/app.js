898384cdce1f09959157869c0e2c7fd92844680e
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-07-29 17:31:41 +0300

    rm csrf (use CSP instead)

--- src/server/app.js
+++ src/server/app.js
@@ -6,7 +6,6 @@ const cookieParser = require("cookie-parser");
 const setupSecurity = require("./security");
 const morganLogs = require("./security/morgan.js");
 require("./security/morgan.js");
-const CSRF = require("csrf");
 const app = express();
 
 // Примените настройки безопасности
@@ -18,29 +17,9 @@ morganLogs(app);
 app.use(express.json());
 app.use(cookieParser());
 
-const csrfProtection = new CSRF();
-
-app.use((req, res, next) => {
-  if (["POST", "PUT", "DELETE"].includes(req.method)) {
-    // Для определенных методов проверяем CSRF токен
-    const token =
-      req.body._csrf || req.query._csrf || req.headers["x-csrf-token"];
-    if (!csrfProtection.verify(req.cookies._csrf, token)) {
-      return res.status(403).send("CSRF token mismatch");
-    }
-  }
-  next();
-});
-
 app.use("/users", authRoutes);
 app.use("/referral", refRoutes);
 app.use("/cookies", cookiesRoutes);
-
-app.get("/csrf-token", (req, res) => {
-  const token = csrfProtection.create(req.cookies._csrf);
-  res.cookie("_csrf", token);
-  res.json({ token });
-});
 
 // app.use((err, req, res, next) => {
 //   if (err.code !== "EBADCSRFTOKEN") return next(err);
