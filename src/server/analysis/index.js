9af21aa128a4af3795e80aed649be62e07e94efb
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-02 22:18:40 +0300

    add country metric

--- src/server/analysis/index.js
+++ src/server/analysis/index.js
@@ -3,75 +3,82 @@ const sendMail = require("../utils/mailer.js");
 
 async function calculateMetricsAndSendEmail() {
   const analysisEmail = process.env.ANALYSIS_EMAIL;
+
   try {
+    // Define the date boundaries
     const lastMonth = new Date();
     lastMonth.setMonth(lastMonth.getMonth() - 1);
+    const lastWeek = new Date();
+    lastWeek.setDate(lastWeek.getDate() - 7);
 
-    // Загальне число користувачів
-    const totalUsers = await User.countDocuments();
-    const totalUsersLastMonth = await User.countDocuments({
-      registrationDate: { $lt: lastMonth },
+    const uniqueCountriesLastWeek = await User.distinct("country", {
+      registrationDate: { $gte: lastWeek },
     });
-    const growthTotalUsers =
-      totalUsersLastMonth > 0
-        ? (totalUsers / totalUsersLastMonth - 1) * 100
-        : 0;
 
-    // Шкільні користувачі
-    const schoolUsers = await User.countDocuments({
-      "subscription.type": "School",
-    });
-    const schoolUsersLastMonth = await User.countDocuments({
-      "subscription.type": "School",
-      registrationDate: { $lt: lastMonth },
-    });
-    const growthSchoolUsers =
-      schoolUsersLastMonth > 0
-        ? (schoolUsers / schoolUsersLastMonth - 1) * 100
-        : 0;
+    const countriesList = uniqueCountriesLastWeek.join(", ");
 
-    // Звичайні користувачі
-    const commonUsers = await User.countDocuments({
-      "subscription.type": "Common",
-    });
-    const commonUsersLastMonth = await User.countDocuments({
-      "subscription.type": "Common",
-      registrationDate: { $lt: lastMonth },
-    });
-    const growthCommonUsers =
-      commonUsersLastMonth > 0
-        ? (commonUsers / commonUsersLastMonth - 1) * 100
-        : 0;
+    const topCountryData = await User.aggregate([
+      { $group: { _id: "$country", count: { $sum: 1 } } },
+      { $sort: { count: -1 } },
+      { $limit: 1 },
+    ]);
+    const topCountry = topCountryData[0]?._id || "Unknown";
 
-    // Шкільні платні користувачі
-    const schoolPremiumUsers = await User.countDocuments({
-      "subscription.type": "School",
-      "subscription.isPremium": true,
-    });
-    const schoolPremiumUsersLastMonth = await User.countDocuments({
-      "subscription.type": "School",
-      "subscription.isPremium": true,
-      registrationDate: { $lt: lastMonth },
-    });
-    const growthSchoolPremiumUsers =
-      schoolPremiumUsersLastMonth > 0
-        ? (schoolPremiumUsers / schoolPremiumUsersLastMonth - 1) * 100
-        : 0;
+    // Define a function to calculate growth
+    const calculateGrowth = (current, previous) =>
+      previous > 0 ? (current / previous - 1) * 100 : 0;
 
-    // Звичайні платні користувачі
-    const commonPremiumUsers = await User.countDocuments({
-      "subscription.type": "Common",
-      "subscription.isPremium": true,
-    });
-    const commonPremiumUsersLastMonth = await User.countDocuments({
-      "subscription.type": "Common",
-      "subscription.isPremium": true,
-      registrationDate: { $lt: lastMonth },
-    });
-    const growthCommonPremiumUsers =
-      commonPremiumUsersLastMonth > 0
-        ? (commonPremiumUsers / commonPremiumUsersLastMonth - 1) * 100
-        : 0;
+    // Fetch user statistics
+    const [
+      totalUsers,
+      totalUsersLastMonth,
+      schoolUsers,
+      schoolUsersLastMonth,
+      commonUsers,
+      commonUsersLastMonth,
+      schoolPremiumUsers,
+      schoolPremiumUsersLastMonth,
+      commonPremiumUsers,
+      commonPremiumUsersLastMonth,
+    ] = await Promise.all([
+      User.countDocuments(),
+      User.countDocuments({ registrationDate: { $lt: lastMonth } }),
+      User.countDocuments({ "subscription.type": "School" }),
+      User.countDocuments({
+        "subscription.type": "School",
+        registrationDate: { $lt: lastMonth },
+      }),
+      User.countDocuments({ "subscription.type": "Common" }),
+      User.countDocuments({
+        "subscription.type": "Common",
+        registrationDate: { $gte: lastMonth },
+      }),
+      User.countDocuments({
+        "subscription.type": "School",
+        "subscription.isPremium": true,
+      }),
+      User.countDocuments({
+        "subscription.type": "School",
+        "subscription.isPremium": true,
+        registrationDate: { $lt: lastMonth },
+      }),
+      User.countDocuments({
+        "subscription.type": "Common",
+        "subscription.isPremium": true,
+      }),
+      User.countDocuments({
+        "subscription.type": "Common",
+        "subscription.isPremium": true,
+        registrationDate: { $lt: lastMonth },
+      }),
+    ]);
+
+    // Calculate growth for each metric
+    const growthTotalUsers = calculateGrowth(totalUsers, totalUsersLastMonth); // prettier-ignore
+    const growthSchoolUsers = calculateGrowth(schoolUsers, schoolUsersLastMonth); // prettier-ignore
+    const growthCommonUsers = calculateGrowth(commonUsers, commonUsersLastMonth); // prettier-ignore
+    const growthSchoolPremiumUsers = calculateGrowth(schoolPremiumUsers, schoolPremiumUsersLastMonth); // prettier-ignore
+    const growthCommonPremiumUsers = calculateGrowth(commonPremiumUsers, commonPremiumUsersLastMonth); // prettier-ignore
 
     // Формування листа
     const subject = "Analytic mail";
@@ -97,6 +104,11 @@ async function calculateMetricsAndSendEmail() {
     <h2>Звичайні платні користувачі</h2>
     <p>Кількість: ${commonPremiumUsers}</p>
     <p>Приріст: ${growthCommonPremiumUsers.toFixed(2)}%</p>
+
+    <h2>Статистика по странам</h2>
+    <p>Більше всього користувачів з країни: ${topCountry}</p>
+    <p>На протязі останнього тижня зареєстровано користувачів з наступних краiн: ${countriesList}</p>
+
     `;
 
     await sendMail(analysisEmail, "Analytic", subject, HTML);
