e42e1788e9a5cb1808df8f9c70b25dab6820e209
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-04 01:27:28 +0300

    use configured axios instead of simple axios

--- src/redux/lessons/lessons-operations.js
+++ src/redux/lessons/lessons-operations.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios from "../../config/axios";
 
 const fetchLessonsPointsTotalSum = createAsyncThunk(
   "lessons/fetchTotalPointsSum",
