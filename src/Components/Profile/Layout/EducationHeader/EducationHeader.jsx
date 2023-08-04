751966af5194650bdcb340ab9b2af0f90c9a2af2
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-05 00:11:05 +0300

    add useSelector and check is user logged in

--- src/Components/Profile/Layout/EducationHeader/EducationHeader.jsx
+++ src/Components/Profile/Layout/EducationHeader/EducationHeader.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
 import LogoLink from "../../../Header/HomeHeader/Navigation/Links/LogoLink";
 import Avatar from "../Header/Avatar/Avatar";
 
 import "./EducationHeader.scss";
+import { fetchLessonsPointsTotalSum } from "../../../../redux/lessons/lessons-operations";
+import { useDispatch, useSelector } from "react-redux";
+import { selectLessonsTotalPoints } from "../../../../redux/lessons/lessons-selectors";
+import { selectIsLoggedIn } from "../../../../redux/auth/auth-selectors";
 
 const EducationHeader = () => {
-  const points = 6876;
+  const dispatch = useDispatch();
+  const points = useSelector(selectLessonsTotalPoints);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  useEffect(() => {
+    isLoggedIn && dispatch(fetchLessonsPointsTotalSum());
+  }, [dispatch, isLoggedIn]);
+
   return (
     <>
       <>
