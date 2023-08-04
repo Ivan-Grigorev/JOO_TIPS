70314d4549a98011dcf91f0de62683c5199ee1af
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-05 02:44:40 +0300

    add react-calendar

--- src/Components/Profile/EducationContent/Lessons/Lessons.jsx
+++ src/Components/Profile/EducationContent/Lessons/Lessons.jsx
@@ -1,7 +1,14 @@
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+import './Lessons-calendar.scss'
+
 const Lessons = () => {
   return (
     <>
-     <p>Here'd be Lessons</p>
+      <Calendar
+        calendarType="gregory" // используйте "US" для начала недели с воскресенья
+        locale="en-US" // используйте "en-US" для отображения дней недели на английском языке
+      />
     </>
   );
 };
