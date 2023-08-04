11cf9623ed05fd1b498cb060639236eba9fc0b20
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-05 01:07:28 +0300

    add base markup

--- src/Components/Profile/Layout/EducationFooter/EducationFooter.jsx
+++ src/Components/Profile/Layout/EducationFooter/EducationFooter.jsx
@@ -1,7 +1,23 @@
+import "./EducationFooter.scss";
+import topicsIcon from "./icons/topics-icon.svg";
+import lessonsIcon from "./icons/lessons-icon.svg";
+import resultsIcon from "./icons/results-icon.svg";
+
 const EducationFooter = () => {
   return (
-    <footer>
-      <></>
+    <footer className="education-footer">
+      <buttons className="education-footer__buttons">
+        <img src={topicsIcon} alt="Topics" />
+        Topics
+      </buttons>
+      <buttons className="education-footer__buttons">
+        <img src={lessonsIcon} alt="Lessons" />
+        Lessons
+      </buttons>
+      <buttons className="education-footer__buttons">
+        <img src={resultsIcon} alt="Results" />
+        Results
+      </buttons>
     </footer>
   );
 };
