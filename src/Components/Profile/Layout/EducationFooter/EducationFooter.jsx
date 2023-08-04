c2ddcebf0fe4b8b9be7fc8d4f669c459361e2ab0
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-05 01:57:23 +0300

    upd handlers

--- src/Components/Profile/Layout/EducationFooter/EducationFooter.jsx
+++ src/Components/Profile/Layout/EducationFooter/EducationFooter.jsx
@@ -3,18 +3,27 @@ import topicsIcon from "./icons/topics-icon.svg";
 import lessonsIcon from "./icons/lessons-icon.svg";
 import resultsIcon from "./icons/results-icon.svg";
 
-const EducationFooter = () => {
+const EducationFooter = ({ handleButtonClick }) => {
   return (
     <footer className="education-footer">
-      <buttons className="education-footer__buttons">
+      <buttons
+        className="education-footer__buttons"
+        onClick={() => handleButtonClick("Topics")}
+      >
         <img src={topicsIcon} alt="Topics" />
         Topics
       </buttons>
-      <buttons className="education-footer__buttons">
+      <buttons
+        className="education-footer__buttons"
+        onClick={() => handleButtonClick("Lessons")}
+      >
         <img src={lessonsIcon} alt="Lessons" />
         Lessons
       </buttons>
-      <buttons className="education-footer__buttons">
+      <buttons
+        className="education-footer__buttons"
+        onClick={() => handleButtonClick("Results")}
+      >
         <img src={resultsIcon} alt="Results" />
         Results
       </buttons>
