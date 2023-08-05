aa1453e0dbcf0e93ad8bb37d4ed56729e72ea533
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-05 17:09:47 +0300

    rework as a react components

---
+++ src/Components/Profile/Layout/EducationFooter/icons/TopicsIcon.jsx
@@ -0,0 +1,54 @@
+const TopicsIcon = () => {
+  return (
+    <svg
+      width="27"
+      height="24"
+      viewBox="0 0 27 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.90039 5C4.90039 4.44772 5.38881 4 5.9913 4H10.3549C10.9574 4 11.4458 4.44772 11.4458 5V9C11.4458 9.55228 10.9574 10 10.3549 10H5.9913C5.38881 10 4.90039 9.55228 4.90039 9V5Z"
+        stroke="#C8C8C8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon-stroke"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.90039 15C4.90039 14.4477 5.38881 14 5.9913 14H10.3549C10.9574 14 11.4458 14.4477 11.4458 15V19C11.4458 19.5523 10.9574 20 10.3549 20H5.9913C5.38881 20 4.90039 19.5523 4.90039 19V15Z"
+        stroke="#C8C8C8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon-stroke"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.8095 5C15.8095 4.44772 16.2979 4 16.9004 4H21.264C21.8665 4 22.3549 4.44772 22.3549 5V9C22.3549 9.55228 21.8665 10 21.264 10H16.9004C16.2979 10 15.8095 9.55228 15.8095 9V5Z"
+        stroke="#C8C8C8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon-stroke"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.8095 15C15.8095 14.4477 16.2979 14 16.9004 14H21.264C21.8665 14 22.3549 14.4477 22.3549 15V19C22.3549 19.5523 21.8665 20 21.264 20H16.9004C16.2979 20 15.8095 19.5523 15.8095 19V15Z"
+        stroke="#C8C8C8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon-stroke"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default TopicsIcon;
