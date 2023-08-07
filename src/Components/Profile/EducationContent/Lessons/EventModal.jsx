26eb45caa72d43e725b219bc2a36d12a0051565b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-07 16:23:02 +0300

    imported styles

--- src/Components/Profile/EducationContent/Lessons/EventModal.jsx
+++ src/Components/Profile/EducationContent/Lessons/EventModal.jsx
@@ -7,17 +7,25 @@ import {
   ModalBody,
 } from "@chakra-ui/react";
 
+import "./eventModal.scss";
+
 const EventModal = ({ event, isOpen, onClose }) => {
   console.log(event);
+
+  const { startTime, endTime, topic, subtopic, lessonNumber, flashcardsCount } =
+    event;
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{event.title}</ModalHeader>
+        <ModalHeader>
+          <h1 className="calendar-modal__topic">{topic}</h1>
+          <span className="calendar-modal__subtopic">{subtopic}</span>
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <p>{`Start: ${event.start.toLocaleString()}`}</p>
-          <p>{`End: ${event.end.toLocaleString()}`}</p>
+          <p>{`Start: ${startTime.toLocaleString()}`}</p>
+          <p>{`End: ${endTime.toLocaleString()}`}</p>
           {/* Добавьте другие поля для отображения дополнительной информации о событии */}
         </ModalBody>
       </ModalContent>
