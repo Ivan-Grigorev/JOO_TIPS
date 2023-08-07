c5e4590f124da920d4ae94da1827e714c90bdba8
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-07 15:44:44 +0300

    upd lesson model

--- src/server/models/lessons/lessons.js
+++ src/server/models/lessons/lessons.js
@@ -3,27 +3,82 @@ const mongoose = require("mongoose");
 const lessonSchema = new mongoose.Schema(
   {
     userId: {
-      type: mongoose.Schema.Types.ObjectId, // используется для ссылки на другую коллекцию (в данном случае, предположительно на пользователей)
-      ref: "User", // имя модели, на которую ссылаемся
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: [true, "UserID is required"],
     },
-    points: {
+    sessionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Session",
+      required: [true, "SessionID is required"],
+    },
+    lessonTypeId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "LessonType",
+      required: [true, "LessonTypeID is required"],
+    },
+    startTime: {
+      type: Date,
+      required: [true, "Start time is required."],
+    },
+    endTime: {
+      type: Date,
+      required: [true, "End time is required."],
+    },
+    flashcardsResult: {
+      type: [
+        {
+          flashcardId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Flashcard",
+          },
+          points: {
+            type: Number,
+            default: 0,
+          },
+          duration: {
+            type: Number,
+            default: 0,
+          },
+          viewsCount: {
+            type: Number,
+            default: 0,
+          },
+        },
+      ],
+      default: [],
+    },
+    testsResult: {
+      type: [
+        {
+          testId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Test",
+          },
+          points: {
+            type: Number,
+            default: 0,
+          },
+          duration: {
+            type: Number,
+            default: 0,
+          },
+        },
+      ],
+      default: [],
+    },
+    totalPoints: {
       type: Number,
-      required: [true, "Lesson points is required."],
       default: 0,
     },
-    title: {
-      // добавим заголовок урока для примера
-      type: String,
-      required: [true, "Lesson title is required."],
-      trim: true,
+    lessonNumber: {
+      type: Number,
+      required: [true, "Lesson number is required."],
     },
-    dateCompleted: {
-      // дата завершения урока, это может быть полезной информацией
-      type: Date,
-      default: Date.now,
+    isCompleted: {
+      type: Boolean,
+      default: false,
     },
-    // ... другие поля, которые вы можете добавить
   },
   { versionKey: false }
 );
