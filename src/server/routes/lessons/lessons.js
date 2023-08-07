44fadec1ce3422c5e7140acf4291f1def225d493
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-07 17:09:12 +0300

    add function getLessons

--- src/server/routes/lessons/lessons.js
+++ src/server/routes/lessons/lessons.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const middlewares = require("../../middlewares/auth.js");
-const { getLessonsPointsSum } = require("../../controllers/lessons/lessons.js");
+const {
+  getLessonsPointsSum,
+  getLessons,
+} = require("../../controllers/lessons/lessons.js");
 
 const router = express.Router();
 
+router.get("", middlewares.auth, getLessons);
+
 router.get("/points", middlewares.auth, getLessonsPointsSum);
+
 
 module.exports = router;
