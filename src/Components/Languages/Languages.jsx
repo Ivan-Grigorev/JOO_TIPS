ae256ecda95c5b4c9246d79b75f6d334efc3ac96
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-09 01:59:36 +0300

    create base markup for language choose page

---
+++ src/Components/Languages/Languages.jsx
@@ -0,0 +1,45 @@
+import "./Languages.scss";
+const Languages = () => {
+  return (
+    <>
+      <div className="languages">
+        <ul className="languages-list">
+          <li>
+            <p>JavaScript</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li>
+            <p>Python</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li>
+            <p>Java</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li>
+            <p>SWIFT</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li>
+            <p>C#</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li>
+            <p>C++</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li>
+            <p>Golang</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li>
+            <p>PHP</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Languages;
