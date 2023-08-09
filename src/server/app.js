619dc4fb941a031fa149c8255b19d064a9f106d4
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-09 13:53:45 +0300

    add languages routes

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,7 @@ const authRoutes = require("./routes/auth/auth");
 const refRoutes = require("./routes/referral");
 const cookiesRoutes = require("./routes/cookies");
 const lessonsRoutes = require("./routes/lessons/lessons");
+const languagesRoutes = require("./routes/languages/languages");
 
 const cookieParser = require("cookie-parser");
 const setupSecurity = require("./security");
@@ -23,6 +24,7 @@ app.use("/users", authRoutes);
 app.use("/lessons", lessonsRoutes);
 app.use("/referral", refRoutes);
 app.use("/cookies", cookiesRoutes);
+app.use("/languages", languagesRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
