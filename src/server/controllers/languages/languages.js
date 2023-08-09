91d704d9c68037cbd662dfa6ac98f2f17fd2c3b1
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-09 13:54:26 +0300

    add language controller (get and add functions)

---
+++ src/server/controllers/languages/languages.js
@@ -0,0 +1,31 @@
+const User = require("../../models/user/user");
+
+async function get(req, res) {
+  try {
+    const user = await User.findById(req.user.id);
+    res.status(200).json(user.languages);
+  } catch (e) {
+    console.error(`Error getting user languages: ${e.message}`);
+    res.status(500).json({ message: "Error getting user languages." });
+  }
+}
+
+async function add(req, res) {
+  try {
+    const user = await User.findById(req.user.id);
+
+    const newLanguage = req.body.language; // Язык из тела запроса
+    if (!newLanguage) return res.status(400).json({ message: "Language is required." }); // prettier-ignore
+
+    user.languages.push(newLanguage); // Добавление языка в массив
+    await user.save(); // Сохранение изменений
+
+    console.log(`Language ${newLanguage} added to user ${user.name}`);
+    res.status(200).json(user.languages);
+  } catch (e) {
+    console.log(`Error while adding language to user object: ${e}`);
+    res.status(500).json({ message: e.message });
+  }
+}
+
+module.exports = { get, add };
