8f9aeb9418b87148c60273be75e8573f9c970bb8
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-09 20:20:33 +0300

    add onClick function (choose language)

--- src/Components/Languages/LanguageCard.jsx
+++ src/Components/Languages/LanguageCard.jsx
@@ -1,17 +1,11 @@
-const LanguageCard = ({ iconSrc, altText, languageName }) => {
-  return (
-    <div className="card-wrapper">
-      <div className="card">
-        <img
-          src={iconSrc}
-          alt={altText}
-          className="programming-language-icon"
-        />
-        <p className="card__text">{languageName}</p>
-        <div className="card-shine"></div>
-      </div>
+const LanguageCard = ({ iconSrc, altText, languageName, chooseLanguage }) => (
+  <div className="card-wrapper">
+    <div className="card" onClick={() => chooseLanguage(languageName)}>
+      <img src={iconSrc} alt={altText} className="programming-language-icon" />
+      <p className="card__text">{languageName}</p>
+      <div className="card-shine"></div>
     </div>
-  );
-};
+  </div>
+);
 
 export default LanguageCard;
