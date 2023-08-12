3e98fa2477eade419a0528306a281b2f98de2f87
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-12 20:07:50 +0300

    create base achievements model

---
+++ src/server/models/achievements/achievements.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const achievementSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: String,
+  dateEarned: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Achievement = mongoose.model("Achievement", achievementSchema);
+
+module.exports = Achievement;
