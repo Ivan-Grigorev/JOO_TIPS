d43d66958a1150d36f45ba91ccebe7328a42a117
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-14 01:09:06 +0300

    add comments

--- src/Components/Profile/Layout/SwipeIndicator/SwipeIndicator.jsx
+++ src/Components/Profile/Layout/SwipeIndicator/SwipeIndicator.jsx
@@ -1,8 +1,10 @@
-import "./SwipeIndicator.scss";
+import "./SwipeIndicator.scss"; // Import the styles for the SwipeIndicator component
 
+// SwipeIndicator component to show indicators based on active education content and achievements page status
 const SwipeIndicator = ({ activeEducationContent, isAchievementsPageOpen }) => {
-  const shallHide = activeEducationContent === "Topics" ? "" : "hided";
+  const shallHide = activeEducationContent === "Topics" ? "" : "hided"; // Conditionally determine if the indicator should be hidden
 
+  // Determine classes to highlight active content based on the current state
   const topicsContent =
     activeEducationContent === "Topics" && isAchievementsPageOpen === false
       ? "active"
@@ -12,12 +14,15 @@ const SwipeIndicator = ({ activeEducationContent, isAchievementsPageOpen }) => {
 
   return (
     <>
+      {/* Container for swipe indicators */}
       <div className={`swipe-indicator ${shallHide}`}>
+        {/* Indicator for achievements content */}
         <div className={achievementsContent}></div>
+        {/* Indicator for topics content */}
         <div className={topicsContent}></div>
       </div>
     </>
   );
 };
 
-export default SwipeIndicator;
+export default SwipeIndicator; // Export the SwipeIndicator component
