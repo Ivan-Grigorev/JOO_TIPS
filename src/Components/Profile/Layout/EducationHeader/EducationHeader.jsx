2113d4ae1002730c9641ec96dd09b7f6ed0c6d95
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-15 16:30:04 +0300

    add height limit to body (also in education header)

--- src/Components/Profile/Layout/EducationHeader/EducationHeader.jsx
+++ src/Components/Profile/Layout/EducationHeader/EducationHeader.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"; // Import the useEffect hook from React
+import { useEffect, useState } from "react"; // Import the useEffect hook from React
 import LogoLink from "../../../Header/HomeHeader/Navigation/Links/LogoLink"; // Import the logo link component
 import Avatar from "../Header/Avatar/Avatar"; // Import the avatar component
 
@@ -15,8 +15,13 @@ import "./Burger-menu.scss";
 import { Link } from "react-router-dom";
 
 const EducationHeader = ({ isAchievementsPageOpen }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const dispatch = useDispatch(); // Initialize the dispatch function from react-redux
+
   const isLoggedIn = useSelector(selectIsLoggedIn); // Get the user's login status using a selector
+  const points = useSelector(selectLessonsTotalPoints); // Get the total lesson points using a selector
+  const activeLanguage = useSelector(selectUserActiveLanguage); // Get the user's active language using a selector
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -39,32 +44,43 @@ const EducationHeader = ({ isAchievementsPageOpen }) => {
     // Check if the achievements page is open
     // If true, add "hide" class; otherwise, remove it
     isAchievementsPageOpen ? setClass() : resetClass();
+  }, [isAchievementsPageOpen]); // The effect will re-run whenever isAchievementsPageOpen changes
 
-    // The effect will re-run whenever isAchievementsPageOpen changes
-  }, [isAchievementsPageOpen]);
+  // Define a function to handle the state change of the menu
+  const handleStateChange = (state) => {
+    // Find the <body> element in the document
+    const body = document.body;
 
-  const points = useSelector(selectLessonsTotalPoints); // Get the total lesson points using a selector
-  const activeLanguage = useSelector(selectUserActiveLanguage); // Get the user's active language using a selector
+    // Determine whether the menu is open
+    const isMenuOpen = state.isOpen;
+
+    // Add or remove the "height-limit" class based on the menu's state
+    body.classList.toggle("height-limit", isMenuOpen);
+
+    // Update the state variable to reflect the menu's open/closed state
+    setIsMenuOpen(isMenuOpen);
+  };
 
   const shallHide = isAchievementsPageOpen === true ? "hide" : ""; // Determine if the header should be hidden on the achievements page
 
-  const options = [];
   const renderItems = () => {
+    const output = [];
+
     for (let index = 0; index < 15; index++) {
-      options.push(
+      output.push(
         <Link to={"#"} key={index}>
           Option {index + 1}
         </Link>
       );
     }
-    return options;
+    return output;
   };
 
   return (
     <>
       <header className={`education-header ${shallHide}`}>
         <div className="education-header__top">
-          <Menu>
+          <Menu isOpen={isMenuOpen} onStateChange={handleStateChange}>
             {/* Content of the burger menu */}
             {renderItems()}
           </Menu>
