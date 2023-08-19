19fe813dfa741d4938afba03d5bf7dbfc9781a9d
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-19 13:44:28 +0300

    optimized useLessons hook

--- src/hooks/useLessons.js
+++ src/hooks/useLessons.js
@@ -5,12 +5,27 @@ import { selectUserLessons } from "../redux/lessons/lessons-selectors";
 import { fetchLessons } from "../redux/lessons/lessons-operations";
 
 export default function useLessons() {
+  // Redux dispatch function
   const dispatch = useDispatch();
+
+  // Retrieve user lessons from Redux store
   const lessons = useSelector(selectUserLessons);
 
-  useEffect(() => {
-    dispatch(fetchLessons());
-  }, [dispatch]);
+  // Retrieve stored lessons from session storage
+  const storedLessons = JSON.parse(sessionStorage.getItem("lessons"));
 
-  return lessons;
+  // Check if stored lessons are not available, and if so, fetch lessons
+  useEffect(() => {
+    if (!storedLessons) dispatch(fetchLessons());
+  }, [dispatch, storedLessons]);
+
+  // Update session storage with lessons if available and not empty
+  useEffect(() => {
+    if (lessons && lessons.length > 0) {
+      sessionStorage.setItem("lessons", JSON.stringify(lessons));
+    }
+  }, [lessons]);
+
+  // Return stored lessons if available, otherwise return fetched lessons
+  return storedLessons || lessons;
 }
