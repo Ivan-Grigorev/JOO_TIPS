2ddb8fd851dbf1c66e4aa5bd23475a1dafb197a6
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-25 16:35:03 +0300

    rework

--- src/Components/Profile/EducationContent/Lessons/CalendarCustomToolbar.jsx
+++ src/Components/Profile/EducationContent/Lessons/CalendarCustomToolbar.jsx
@@ -11,18 +11,18 @@ const CustomToolbar = ({ onNavigate, label }) => {
     <div className="rbc-toolbar">
       <span className="custom-nav">
         {/* Кнопка для перехода к предыдущему месяцу */}
-        <button onClick={() => navigate("PREV")}>{"<"}</button>
+        {/* <button onClick={() => navigate("PREV")}>{"<"}</button> */}
 
         {/* Отображение текущего месяца */}
         <span className="current-month-label">{label}</span>
 
         {/* Кнопка для перехода к следующему месяцу */}
-        <button onClick={() => navigate("NEXT")}>{">"}</button>
+        {/* <button onClick={() => navigate("NEXT")}>{">"}</button> */}
 
         {/* Кнопка для возвращения к текущему дню */}
-        <button onClick={() => navigate("TODAY")} className="custom-today-btn">
-          {"Today"}
-        </button>
+        {/* <button onClick={() => navigate("TODAY")} className="custom-today-btn"> */}
+          {/* {"Today"} */}
+        {/* </button> */}
       </span>
     </div>
   );
