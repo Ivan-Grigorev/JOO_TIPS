d41c3bdb12964cec8a74a471d1b61ba68f7becb3
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-26 14:51:12 +0300

    done. use memo

--- src/Components/Profile/Layout/EducationHeader/MissedLessonsIndicator/MissedLessonsIndicator.jsx
+++ src/Components/Profile/Layout/EducationHeader/MissedLessonsIndicator/MissedLessonsIndicator.jsx
@@ -1,18 +1,20 @@
 import { useSelector } from "react-redux";
 import { selectMissedLessonsType } from "../../../../../redux/lessons/lessons-selectors";
 import "./MissedLessonsIndicator.scss";
+import { memo } from "react";
 
 const MissedLessonsIndicator = () => {
   const missedLessons = useSelector(selectMissedLessonsType);
+
   return (
-    <>
+    <div className="missed-indicator__container">
       {missedLessons.daily && <div className="missed-indicator daily"></div>}
       {missedLessons.weekly && <div className="missed-indicator weekly"></div>}
       {missedLessons.monthly && (
         <div className="missed-indicator monthly"></div>
       )}
-    </>
+    </div>
   );
 };
 
-export default MissedLessonsIndicator;
+export default memo(MissedLessonsIndicator);
