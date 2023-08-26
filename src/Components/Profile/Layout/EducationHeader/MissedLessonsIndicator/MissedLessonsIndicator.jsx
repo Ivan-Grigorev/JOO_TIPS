780460f7406e5b2fed1a7960331956262ece484d
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-26 14:24:17 +0300

    add render condition

--- src/Components/Profile/Layout/EducationHeader/MissedLessonsIndicator/MissedLessonsIndicator.jsx
+++ src/Components/Profile/Layout/EducationHeader/MissedLessonsIndicator/MissedLessonsIndicator.jsx
@@ -1,9 +1,16 @@
+import { useSelector } from "react-redux";
+import { selectMissedLessonsType } from "../../../../../redux/lessons/lessons-selectors";
 import "./MissedLessonsIndicator.scss";
 
 const MissedLessonsIndicator = () => {
+  const missedLessons = useSelector(selectMissedLessonsType);
   return (
     <>
-      <></>
+      {missedLessons.daily && <div className="missed-indicator daily"></div>}
+      {missedLessons.weekly && <div className="missed-indicator weekly"></div>}
+      {missedLessons.monthly && (
+        <div className="missed-indicator monthly"></div>
+      )}
     </>
   );
 };
