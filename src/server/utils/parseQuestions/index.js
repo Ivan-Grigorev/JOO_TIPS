668638f2fd1c59976e58ef9155e6fcc1510fc74f
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-27 18:22:03 +0300

    get difficult, isCorrect and optionText from parsed datas

--- src/server/utils/parseQuestions/index.js
+++ src/server/utils/parseQuestions/index.js
@@ -43,29 +43,91 @@ async function getDataFromGoogleDocs() {
 async function parseAndSaveData() {
   const rows = await getDataFromGoogleDocs();
 
-  // Создание нового Excel-файла и листа
-  const workbook = new excel.Workbook();
-  const sheet = workbook.addWorksheet("Sheet1");
+  // Создание объекта для хранения заголовков и соответствующих индексов столбцов
+  const headers = {
+    language: 0,
+    topic: 1,
+    text: 2,
+    example: 3,
+    qas: 4,
+    answers: 5,
+    difficultyLevels: 6,
+    optionText: 7,
+    isCorrect: 8,
+  };
+  // Создание объекта для хранения временных данных по картам
+  const cardData = {};
 
-  console.log(rows[0]);
-  // rows.forEach((row) => {
-  //   const column1Value = row[0];
+  // Пропускаем первую строку, так как это заголовки
+  for (
+    let i = 1;
+    i < 9;
+    // rows.length;
+    i++
+  ) {
+    const row = rows[i];
 
-  //   // Заполните данные в строке листа Excel
-  // });
-  // console.log(column1Value);
-  // // Сохранение Excel-файла
-  // await workbook.xlsx.writeFile("parsed_data.xlsx");
+    // Извлечение данных из соответствующих столбцов на основе индексов из headers
+    const language = row[headers.language];
+    const topic = row[headers.topic];
+    const text = row[headers.text];
+    const example = row[headers.example];
+    const answersParts = row[headers.answers].split(" ");
+    const difficultyLevels = answersParts[0].substring(1, answersParts[0].length - 1); // prettier-ignore
+    const isCorrect = answersParts[1].substring(1, answersParts[1].length - 1);
+    const optionText = answersParts.slice(2).join(" ");
+    // Теперь у вас есть difficultyLevels, isCorrect и optionText для каждой строки
 
-  // // Сохранение данных в MongoDB
-  // rows.forEach(async (row) => {
-  //   const newData = new DataModel({
-  //     // Заполните поля данных на основе row
-  //   });
-  //   await newData.save();
-  // });
+    console.log(`difficultyLevels - ${difficultyLevels}`);
+    console.log(`isCorrect - ${isCorrect}`);
+    console.log(`optionText - ${optionText}`);
 
-  // console.log("Data parsed and saved.");
+
+    // const cardKey = `${language}-${topic}-${text}`;
+    // if (!cardData[cardKey]) {
+    //   cardData[cardKey] = {
+    //     language,
+    //     topic,
+    //     text,
+    //     example,
+    //     qas: [],
+    //   };
+    // }
+
+    // // Создание варианта ответа
+    // const option = {
+    //   text: optionText,
+    //   isCorrect,
+    // };
+
+    // // Добавление варианта ответа в соответствующий уровень сложности
+    // const question = cardData[cardKey].qas.find(
+    //   (qa) => qa.questionText === questionText
+    // );
+    // if (question) {
+    //   question.difficultyLevels.medium.push(option); // Выберите сложность по вашему усмотрению
+    // } else {
+    //   cardData[cardKey].qas.push({
+    //     questionText,
+    //     difficultyLevels: {
+    //       easy: [],
+    //       medium: [option], // Выберите сложность по вашему усмотрению
+    //       hard: [],
+    //     },
+    //   });
+    // }
+  }
+
+  // // Сохранение данных в базе данных
+  // for (const cardKey in cardData) {
+  //   const cardInfo = cardData[cardKey];
+
+  //   // Создание новой карты и сохранение в MongoDB
+  //   const card = new Card(cardInfo);
+  //   await card.save();
+  // }
+
+  console.log("Data parsed and saved.");
 }
 
 // Вызов функции для разбора данных
