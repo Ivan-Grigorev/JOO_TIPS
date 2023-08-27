b04a2b8d778d237538a29f7e010a9ce14a3a9a5b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-27 13:59:39 +0300

    create questionModel

---
+++ src/server/models/question/question.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const OptionSchema = new mongoose.Schema({
+  text: String, // текст відповіді
+  isCorrect: Boolean, // позначка, чи є ця відповідь правильною
+});
+
+const questionSchema = new mongoose.Schema({
+  questionText: String, // текст питання
+  cardId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Card",
+  },
+  difficultyLevels: {
+    easy: [OptionSchema], // масив з трьох варіантів відповідей
+    medium: [OptionSchema],
+    hard: [OptionSchema],
+  },
+});
+
+const Question = mongoose.model("Question", questionSchema);
+
+export default Question;
