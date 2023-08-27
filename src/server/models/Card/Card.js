786d9575acd8d8f30cba2d03a2c6c2fb9710803e
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-27 21:14:54 +0300

    upd. models, add required props

--- src/server/models/Card/Card.js
+++ src/server/models/Card/Card.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-const cardSchema = new mongoose.Schema({
-  language: { type: String, required: true },
-  topic: { type: String, required: true },
-  text: { type: String, required: true },
-  example: { type: String, default: null },
-  qas: [{ type: mongoose.Schema.Types.ObjectId, ref: "Question" }],
-});
+const cardSchema = new mongoose.Schema(
+  {
+    language: { type: String, required: true },
+    topic: { type: String, required: true },
+    text: { type: String, required: true },
+    example: { type: String, default: null },
+    qas: [{ type: mongoose.Schema.Types.ObjectId, ref: "Question" }],
+  },
+  { versionKey: false }
+);
 
 const Card = mongoose.model("Card", cardSchema);
 
--- src/server/models/question/question.js
+++ src/server/models/question/question.js
@@ -1,22 +1,32 @@
 const mongoose = require("mongoose");
 
-const OptionSchema = new mongoose.Schema({
-  text: String, // текст відповіді
-  isCorrect: Boolean, // позначка, чи є ця відповідь правильною
-});
+const OptionSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: [true, "Option text is required"] }, // текст відповіді
+    isCorrect: { type: Boolean, required: [true, "IsCorrect is required property"] }, // позначка, чи є ця відповідь правильною
+  },
+  { versionKey: false }
+);
 
-const QuestionSchema = new mongoose.Schema({
-  questionText: String, // текст питання
-  cardId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Card",
+const QuestionSchema = new mongoose.Schema(
+  {
+    questionText: {
+      type: String,
+      required: [true, "Question text is required"],
+    }, // текст питання
+    cardId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Card id is required"],
+      ref: "Card",
+    },
+    difficultyLevels: {
+      easy: [OptionSchema], // масив з трьох варіантів відповідей
+      medium: [OptionSchema],
+      hard: [OptionSchema],
+    },
   },
-  difficultyLevels: {
-    easy: [OptionSchema], // масив з трьох варіантів відповідей
-    medium: [OptionSchema],
-    hard: [OptionSchema],
-  },
-});
+  { versionKey: false }
+);
 
 const Question = mongoose.model("Question", QuestionSchema);
 const QuestionOption = mongoose.model("QuestionOption", OptionSchema);
