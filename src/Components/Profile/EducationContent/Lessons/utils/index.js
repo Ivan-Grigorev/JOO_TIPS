5e11731a12a89346c0eb56d49dbba99125431dea
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-27 12:00:25 +0300

    fix bug with weeks

--- src/Components/Profile/EducationContent/Lessons/utils/index.js
+++ src/Components/Profile/EducationContent/Lessons/utils/index.js
@@ -1,29 +1,59 @@
 import moment from "moment";
+// Добавим константы для радиусов дат
+const DAILY_RADIUS = 2;
+const WEEKLY_RADIUS = 7;
 
 // Function to determine the type of missed lessons
 export function getMissedType(missed) {
   const output = {
-    daily: false,
+    // daily: false,
     weekly: false,
-    monthly: false,
+    // monthly: false,
   };
 
-  // Check for monthly missed lesson
-  const monthly = missed.find(isLastDayOfMonth);
-  if (monthly) output.monthly = true;
+  console.log(missed);
 
-  const weekly = missed.filter(findMissedWeekLesson);
+  // Check for monthly missed lesson
+  // const monthly = missed.find(isLastDayOfMonth);
+  // if (monthly) output.monthly = true;
+
+  const weekly = findMissedWeekLessons(missed);
   if (weekly.length > 0) output.weekly = true;
 
   // Default to daily missed lesson
-  const dailyMissed = findMissedInLast2Days(missed);
+  // const dailyMissed = findMissedInLastDays(missed, DAILY_RADIUS);
 
-  if (dailyMissed.length > 0) {
-    output.daily = true;
-  }
+  // if (dailyMissed.length > 0) {
+  //   output.daily = true;
+  // }
 
   return output;
 }
+
+export const findMissedWeekLessons = (lessons) => {
+  const currentSunday = moment().startOf("week");
+
+  return lessons.filter((day) => {
+    const dayMoment = moment(day);
+    const diff = dayMoment.diff(currentSunday, "days");
+
+    return diff >= 0 && diff <= 7 && dayMoment.day() === 0;
+  });
+};
+
+export const findMissedInLastDays = (lessons, days) => {
+  const today = moment();
+
+  return lessons.filter((day) => {
+    const dayMoment = moment(day);
+
+    // Check if the day is within the specified radius
+    return (
+      today.diff(dayMoment, "days") >= 0 &&
+      today.diff(dayMoment, "days") <= days
+    );
+  });
+};
 
 // Function to check if a date is a Sunday
 export const isSunday = (date) => {
@@ -35,36 +65,4 @@ export const isSunday = (date) => {
 export const isLastDayOfMonth = (inputDate) => {
   const nextDay = moment(inputDate).add(1, "day");
   return !nextDay.isSame(inputDate, "month");
-};
-
-const missedToday = (day) => {
-  const today = moment();
-  const dayMoment = moment(day);
-  return dayMoment.isSame(today, "day");
-};
-
-const findMissedWeekLesson = (day) => {
-  // Check for weekly missed lesson
-  const currentSunday = moment().startOf("week"); // Находим воскресение текущей недели
-
-  const dayMoment = moment(day);
-
-  // Сравниваем с воскресеньем текущей недели
-  const diff = dayMoment.diff(currentSunday, "days");
-
-  return diff >= 0 && diff <= 7 && dayMoment.day() === 0;
-};
-
-// Проверка диапазона дат
-const isMissedInLast2Days = (day) => {
-  const today = moment();
-  const dayMoment = moment(day);
-
-  const diff = today.diff(dayMoment, "days");
-
-  return diff >= 0 && diff <= 2;
-};
-
-const findMissedInLast2Days = (lessons) => {
-  return lessons.filter(isMissedInLast2Days);
 };
