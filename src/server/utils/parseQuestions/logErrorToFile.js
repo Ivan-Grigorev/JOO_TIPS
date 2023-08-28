348e12457c002aefdbb56ff98b0dfbc662d2278d
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-28 16:03:44 +0300

    create logError function to debug

---
+++ src/server/utils/parseQuestions/logErrorToFile.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+const errorFilePath = path.join(__dirname, "error.txt");
+
+// Функция для записи ошибки в файл
+function logErrorToFile(error) {
+  fs.appendFile(errorFilePath, `${error}\n`, (err) => {
+    if (err) {
+      console.error("Не удалось записать ошибку в файл:", err);
+    }
+  });
+}
+
+module.exports = logErrorToFile;
