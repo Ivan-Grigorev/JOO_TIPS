496641efef7a0ec43b084733c451789f2e9726d7
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-08-31 23:05:03 +0300

    rework code errors system, when occurred an error - creates file error[range].log

--- src/server/utils/parseQuestions/logErrorToFile.js
+++ src/server/utils/parseQuestions/logErrorToFile.js
@@ -1,10 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 
-const errorFilePath = path.join(__dirname, "error.log");
+const errorLogsDir = path.join(__dirname, "errorLogs");
+
+// Создаем директорию для логов ошибок, если еще не создана
+if (!fs.existsSync(errorLogsDir)) {
+  fs.mkdirSync(errorLogsDir);
+}
 
 // Функция для записи ошибки в файл
-function logErrorToFile(error) {
+function logErrorToFile(range, error) {
+  const errorFileName = `${range}.error.log`;
+  const errorFilePath = path.join(errorLogsDir, errorFileName);
+
   fs.appendFile(errorFilePath, `${error}\n`, (err) => {
     if (err) {
       console.error("Не удалось записать ошибку в файл:", err);
