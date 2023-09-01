5e494a2154fe9b870c0d6d1ab86652c72c335ab6
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-01 18:01:44 +0300

    add language to error-file

--- src/server/utils/parseQuestions/logErrorToFile.js
+++ src/server/utils/parseQuestions/logErrorToFile.js
@@ -9,8 +9,8 @@ const folderIsNotExist = !fs.existsSync(errorLogsDir);
 if (folderIsNotExist) fs.mkdirSync(errorLogsDir);
 
 // Функция для записи ошибки в файл
-function logErrorToFile(range, error) {
-  const errorFileName = `${range}.error.log`;
+function logErrorToFile(language, range, error) {
+  const errorFileName = `${language}-${range}.error.log`;
   const errorFilePath = path.join(errorLogsDir, errorFileName);
 
   fs.appendFile(errorFilePath, `${error}\n`, (err) => {
