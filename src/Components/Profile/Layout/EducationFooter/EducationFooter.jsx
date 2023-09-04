dc5e56ea004536135a6d500c6ae5e86ec6bc5c38
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-04 12:54:29 +0300

    upd. lesson footer markup and styles

--- src/Components/Profile/Layout/EducationFooter/EducationFooter.jsx
+++ src/Components/Profile/Layout/EducationFooter/EducationFooter.jsx
@@ -8,6 +8,11 @@ import CompetitionIcon from "./icons/CompetitionIcon";
 import { NavLink, useLocation } from "react-router-dom";
 import MissedLessonsIndicator from "../EducationHeader/MissedLessonsIndicator/MissedLessonsIndicator";
 
+import {
+  HiOutlineChevronDoubleLeft,
+  HiOutlineChevronDoubleRight,
+} from "react-icons/hi";
+
 // EducationFooter Component - Represents the footer section of the education page.
 const EducationFooter = () => {
   // Обновляем класс shallHide при изменении isAchievementsPageOpen
@@ -30,6 +35,9 @@ const EducationFooter = () => {
       : setClassName("education-footer");
   }, [location]);
 
+  const onLeftSwipe = () => {};
+  const onRightSwipe = () => {};
+
   // Function to render individual buttons with corresponding icons and labels.
   // Takes in a content key (to determine the active state), the icon component, and the label.
   const renderButton = useCallback((IconComponent, label) => {
@@ -49,7 +57,19 @@ const EducationFooter = () => {
   return (
     <footer className={`${className} ${hideClass} `}>
       {showLessonFooter ? (
-        <></>
+        <div className="container">
+          <HiOutlineChevronDoubleLeft
+            className="swipe-buttons"
+            onClick={onLeftSwipe}
+          />
+
+          <p className="leson__footer-content">Lorem ipsum dolor sit amet.</p>
+
+          <HiOutlineChevronDoubleRight
+            className="swipe-buttons"
+            onClick={onRightSwipe}
+          />
+        </div>
       ) : (
         <>
           {/* Render the Topics, Lessons, and Results buttons */}
