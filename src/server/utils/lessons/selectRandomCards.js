6fc9b72dfe6498091a50b4cbd3fdec786082cdf0
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-16 20:00:39 +0300

    upd. function.; Add case if topics is more than 1 item

--- src/server/utils/lessons/selectRandomCards.js
+++ src/server/utils/lessons/selectRandomCards.js
@@ -2,30 +2,42 @@ const mongoDB = require("../../db");
 const Card = require("../../models/Card/Card");
 const Algorithm = require("./Algorithm");
 
-const selectRandomCards = async () => {
+const selectRandomCards = async (topics) => {
   const db = await mongoDB();
+  try {
+    // TODO оптимизировать через Promise.All()
+    // беру нужные параметры из тех.коллекций
+    const techTopics = await db
+      .collection("topics_to_choose_amount")
+      .findOne({});
+    const chances = await db.collection("topics_chances").findOne({});
 
-  // TODO оптимизировать через Promise.All()
-  // беру нужные параметры из тех.коллекций
-  const techTopics = await db.collection("topics_to_choose_amount").findOne({});
-  const chances = await db.collection("topics_chances").findOne({});
+    const allCardIDs = [];
 
-  const topic = `JavaScript Basics: Understanding variables, data types, and functions.`;
+    for (const topic of topics) {
+      const cardIDs = await Card.find({ topic }, "_id"); // поиск карточек по заданной теме. Возвращает их _id
+      allCardIDs.push(...cardIDs);
+    }
 
-  // поиск карточек по заданной теме. Возвращает их _id
-  const cardIDs = await Card.find({ topic }, "_id"); //* 100 карточек
+    console.log("ID's найденных карточек:", allCardIDs);
 
-  const numToSelect = techTopics.topicsToChoose;
+    const numToSelect = techTopics.topicsToChoose;
 
-  console.log("Вероятности из базы данных:", chances);
+    console.log("Вероятности из базы данных:", chances);
 
-  // const randomCards = await Algorithm(cardIDs, numToSelect, chances);
+    // const randomCards = await Algorithm(cardIDs, numToSelect, chances);
 
-  // console.log("Выбранные карточки:", randomCards);
-
-  db.close();
+    // console.log("Выбранные карточки:", randomCards);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    db.close();
+  }
 };
 
-selectRandomCards();
+const topics = [
+  "JavaScript Basics: Understanding variables, data types, and functions.",
+];
+selectRandomCards(topics);
 
 module.exports = selectRandomCards;
