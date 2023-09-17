c372a47233111244a0093ec327c9f0fc30dfcf6c
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-17 23:09:55 +0300

    crush to techProps module-function

--- src/server/utils/lessons/selectRandomCards.js
+++ src/server/utils/lessons/selectRandomCards.js
@@ -2,36 +2,61 @@ const mongoDB = require("../../db");
 const Card = require("../../models/Card/Card");
 const Algorithm = require("./Algorithm");
 
-const selectRandomCards = async (topics) => {
-  const db = await mongoDB();
+const getTechProps = async (db, language) => {
   try {
     // TODO оптимизировать через Promise.All()
 
+    // беру из тех.коллекции количество тем
     const techTopics = await db
       .collection("topics_to_choose_amount")
-      .findOne({}); // беру из тех.коллекции количество тем
-    const chances = await db.collection("topics_chances").findOne({}); // беру из тех.коллекции шансы появления тех или иных карточек
+      .findOne({});
 
-    console.log("Вероятности из базы данных:", chances);
-    const numToSelect = techTopics.topicsToChoose; // беру значение из объекта
+    // беру из тех.коллекции шансы появления тех или иных карточек
+    const chances = await db.collection("topics_chances").findOne({});
+    // console.log("Вероятности из базы данных:", chances);
+
+    // беру значение из объекта
+    const numToSelect = techTopics.topicsToChoose;
+
+    // получаю список тем из динамической коллекции (обновляется при парсинге)
+    const topics = await db
+      .collection("topics_list")
+      .find({ language })
+      .toArray();
+
+    return { topics, numToSelect, chances };
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const selectRandomCards = async () => {
+  const db = await mongoDB();
+  try {
+    const techProps = await getTechProps(db, "javascript");
 
     const cardObjects = [];
 
+    console.log(techProps.topics);
+
     // проходимся по массиву тем, ищем карточки с соответствующими темами
     // добавляем их в массив cardObjects
-    for (const topic of topics) {
-      const findCards = await Card.find({ topic }); // поиск карточек по заданной теме.
+    for (const obj of techProps.topics) {
+      const findCards = await Card.find({ topic: obj.topic }); // поиск карточек по заданной теме.
       cardObjects.push(...findCards);
     }
+
+    return;
     console.log("Количество найденных карточек:", cardObjects.length);
 
     // Извлекаем _id из объектов карточек и преобразуем их в строки
     const cardIDs = cardObjects.map((card) => card._id.toString());
 
     // Вызов функции Algorithm с правильными аргументами
-    const randomCards = Algorithm(cardIDs, numToSelect);
+    const randomCards = Algorithm(cardIDs, techProps.numToSelect);
 
-    console.log("Выбранные карточки:", randomCards);
+    // console.log("Выбранные карточки:", randomCards);
+    return;
   } catch (e) {
     console.error(e);
   } finally {
@@ -39,10 +64,7 @@ const selectRandomCards = async (topics) => {
   }
 };
 
-const topics = [
-  "JavaScript Basics: Understanding variables, data types, and functions.",
-];
-selectRandomCards(topics);
+selectRandomCards();
 
 module.exports = selectRandomCards;
 
