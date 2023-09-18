8d035d05ad284f54de35411b33475030f9420d1d
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-18 17:46:58 +0300

    add createScheduleToEndOfWeek

--- src/server/middlewares/languages.js
+++ src/server/middlewares/languages.js
@@ -1,4 +1,7 @@
+const Lesson = require("../models/lessons/lessons");
 const User = require("../models/user/user");
+const selectRandomCards = require("../utils/lessons/selectRandomCards");
+const moment = require("moment");
 
 // Middleware for checking if a new language is unique for a user
 async function isUniqueLanguage(req, res, next) {
@@ -22,4 +25,56 @@ async function isUniqueLanguage(req, res, next) {
   }
 }
 
-module.exports = { isUniqueLanguage };
+async function createScheduleToEndOfWeek(req, res, next) {
+  try {
+    const { language } = req.body;
+
+    const randomCards = await selectRandomCards(language);
+
+    const user = await User.findById(req.user.id);
+
+    // Получаем текущую дату
+    const currentDate = moment();
+
+    // Создаем массив дней сегодняшнего дня до субботы
+    const days = [];
+    for (let i = 0; i <= 5; i++) {
+      const date = moment(currentDate).add(i, "days");
+      days.push(date);
+    }
+
+    // Создаем массив объектов уроков для каждой карточки и дня
+    const lessonsToCreate = [];
+    randomCards.forEach((card, index) => {
+      days.forEach((day, dayIndex) => {
+        lessonsToCreate.push({
+          userID: user._id,
+          language: language,
+          topic: card.topic, // Используем тему из карточки
+          cardText: card.cardText, // Используем текст карточки
+          cardsAmount: card.cardsAmount, // Используем количество карточек из карточки
+          startTime: null, // Начальное время (по умолчанию)
+          endTime: null, // Конечное время (по умолчанию)
+          status: null, // Статус (по умолчанию)
+          lessonDate: day.toDate(),
+          lessonNumber: index + 1, // Номер урока
+          lessonDuration: 45, // Длительность урока (по умолчанию)
+        });
+      });
+    });
+
+    // Шаг 2: Сохраняем объекты уроков в базе данных
+    const createdLessons = await Lesson.insertMany(lessonsToCreate);
+
+    // Отправляем ответ, если необходимо
+    res.status(200).json({
+      message: "Schedule created successfully",
+      lessons: createdLessons,
+    });
+  } catch (e) {
+    console.error("Error creating user schedule:", e);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+module.exports = { isUniqueLanguage, createScheduleToEndOfWeek };
