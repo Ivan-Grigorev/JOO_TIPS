ede3492e23e2524aceb5e572e4e68c03536427f4
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-21 18:57:21 +0300

    optimized requests to DB

--- src/server/utils/lessons/getTechProps.js
+++ src/server/utils/lessons/getTechProps.js
@@ -1,25 +1,16 @@
 const getTechProps = async (db, language) => {
   try {
-    // TODO оптимизировать через Promise.All()
+    // Оптимизируем запросы через Promise.all()
+    const [techTopics, chances, topics, lessonTechCollection] =
+      await Promise.all([
+        db.collection("topics_to_choose_amount").findOne({}),
+        db.collection("topics_chances").findOne({}),
+        db.collection("topics_list").find({ language }).toArray(),
+        db.collection("lessons_tech").findOne(),
+      ]);
 
-    // беру из тех.коллекции количество тем
-    const techTopics = await db
-      .collection("topics_to_choose_amount")
-      .findOne({});
+    const { topicsToChoose: numToSelect } = techTopics;
 
-    // беру из тех.коллекции шансы появления тех или иных карточек
-    const chances = await db.collection("topics_chances").findOne({});
-
-    // беру значение из объекта
-    const numToSelect = techTopics.topicsToChoose;
-
-    // получаю список тем из динамической коллекции (обновляется при парсинге)
-    const topics = await db
-      .collection("topics_list")
-      .find({ language })
-      .toArray();
-
-    const lessonTechCollection = await db.collection("lessons_tech").findOne();
     const { lessonDuration, cardsAmount, missedLessonRadius, lessonPoints } =
       lessonTechCollection;
 
