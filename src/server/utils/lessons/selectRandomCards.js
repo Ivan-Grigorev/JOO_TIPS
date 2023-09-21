553b283a810964eba05f8b1774b3e7b1aa51ab31
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-21 17:20:39 +0300

    upd., add alreadyTakenTopics function

--- src/server/utils/lessons/selectRandomCards.js
+++ src/server/utils/lessons/selectRandomCards.js
@@ -1,17 +1,22 @@
 const mongoDB = require("../../db");
 const Card = require("../../models/Card/Card");
 const Algorithm = require("./Algorithm");
+const getAllTakenTopics = require("./getAllTakenTopics");
 const getTechProps = require("./getTechProps");
 
-const selectRandomCards = async (language) => {
+const selectRandomCards = async (userId, language) => {
   const db = await mongoDB();
   try {
     const techProps = await getTechProps(db, language);
+
+    // уже зарезервированные карточки
+    const alreadyTakenTopics = await getAllTakenTopics(userId);
 
     const cardObjects = [];
 
     // Проходимся по массиву тем, ищем карточки с соответствующими темами
     // Добавляем их в массив cardObjects
+    // todo пофиксить. Присутствует 3 темы, а должна только одна.
     for (const obj of techProps.topics) {
       const { topic } = obj;
       const findCards = await Card.find({ topic }); // поиск карточек по заданной теме.
@@ -25,16 +30,12 @@ const selectRandomCards = async (language) => {
     // Извлекаем _id из объектов карточек и преобразуем их в строки
     const cardIDs = cardObjects.map((card) => card._id.toString());
 
-    // Вызов функции Algorithm с правильными аргументами
     const randomCards = Algorithm(cardIDs, techProps.numToSelect);
 
-    // console.log("Выбранные карточки:", randomCards);
     return { cards: randomCards, techProps };
   } catch (e) {
     console.error(e);
   }
 };
-
-// selectRandomCards("javascript");
 
 module.exports = selectRandomCards;
