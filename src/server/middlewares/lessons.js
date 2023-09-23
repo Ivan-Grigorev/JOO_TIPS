59fb837d8d3774337db4eade9079246c897778ed
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-23 18:56:09 +0300

    move createSchedule function to middlewares

--- src/server/middlewares/lessons.js
+++ src/server/middlewares/lessons.js
@@ -1,5 +1,7 @@
 const Lesson = require("../models/lessons/lessons");
 const moment = require("moment");
+const User = require("../models/user/user");
+const selectRandomCards = require("../utils/lessons/selectRandomCards");
 
 // Middleware function to check if a lesson exists by its ID
 const isLessonExistById = async (req, res, next) => {
@@ -64,6 +66,53 @@ const isScheduleAlreadyExists = async (req, res, next) => {
     return e;
   }
 };
+
+// todo написать unit-test
+async function createScheduleToEndOfWeek(req, res, next) {
+  try {
+    const userId = req.user.id;
+    const language = req.body.language;
+
+    // Find the user by their identifier
+    const [user, Algorithm] = await Promise.all([
+      User.findById(userId),
+      selectRandomCards(userId, language),
+    ]);
+
+    // Get the current date and day of the week (0 - Sunday, 6 - Saturday)
+    const currentDate = moment();
+    const currentDayOfWeek = currentDate.day();
+
+    // Calculate how many days are left until Saturday
+    const daysRemaining = 6 - currentDayOfWeek;
+
+    const existingLessons = await isScheduleAlreadyExists(userId, currentDate);
+
+    if (existingLessons.length > 0) {
+      console.log("Lessons already exist for this week".red);
+      return "Lessons already exist for this week";
+    }
+
+    const lessonsToCreate = await createLessons(
+      daysRemaining,
+      currentDate,
+      user._id,
+      language,
+      Algorithm.cards,
+      Algorithm.takenCards,
+      Algorithm.techProps
+    );
+
+    // Insert the created lessons into the database
+    const createdLessons = await Lesson.insertMany(lessonsToCreate);
+    console.log(lessonsToCreate.length + " Lessons have been created".green);
+
+    return { createdLessons };
+  } catch (e) {
+    console.error("Error creating user schedule:", e);
+    throw new Error("Error creating user schedule");
+  }
+}
 
 module.exports = {
   isLessonExistById,
