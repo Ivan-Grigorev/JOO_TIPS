2439429c700afbc272fcdceace51ab931374168d
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-24 11:16:51 +0300

    add isScheduleAlreadyExists in lessons route

--- src/server/routes/lessons/lessons.js
+++ src/server/routes/lessons/lessons.js
@@ -10,7 +10,7 @@ const {
 
 const router = express.Router();
 
-router.get("", auth, getLessons);
+router.get("", auth, middlewares.isScheduleAlreadyExists, getLessons);
 
 router.get("/points", auth, getLessonsPointsSum);
 
