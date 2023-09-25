6823cc9aa4dcdf73961c6e1641e97bc2b8d115ca
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-25 21:57:38 +0300

    add function description

--- src/server/utils/lessons/isTodaySunday.js
+++ src/server/utils/lessons/isTodaySunday.js
@@ -1,4 +1,11 @@
-// Function to check if the current day of the week is Sunday
+/**
+ * @function isTodaySunday
+ * @description Checks if the current day of the week is Sunday (7).
+ *
+ * @param {number} currentDayOfWeek - The current day of the week (1 for Monday, 2 for Tuesday, ..., 7 for Sunday).
+ *
+ * @returns {boolean} Returns true if the current day of the week is Sunday (7), otherwise returns false.
+ */
 function isTodaySunday(currentDayOfWeek) {
   if (currentDayOfWeek === 7) {
     // If the current day of the week is 7 (Sunday), return true
