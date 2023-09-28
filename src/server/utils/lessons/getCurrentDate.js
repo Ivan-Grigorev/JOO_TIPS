0a86ff56d4ad3fe24856bae1cc121148061fec85
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-28 21:24:21 +0300

    upd. date formatting, add hour and minutes

--- src/server/utils/lessons/getCurrentDate.js
+++ src/server/utils/lessons/getCurrentDate.js
@@ -6,21 +6,21 @@ const moment = require("moment");
  *
  * @returns {Object} An object containing the following properties:
  *   - currentDate (moment): The current date as a moment.js object.
- *   - formattedCurrentDate (string): The current date in the "DD.MM.YYYY" format.
+ *   - formattedCurrentDate (string): The current date in the "DD.MM.YYYY HH:mm" format.
  *   - currentDayOfWeek (number): The current day of the week (1 for Monday, 2 for Tuesday, ..., 7 for Sunday).
  *   - daysUntilSunday (number): The number of days remaining until Sunday.
  *   - expiredDate (string): A formatted date representing the next day at 3:00 AM.
  */
 function getCurrentDate() {
   const currentDate = moment();
-  const formattedCurrentDate = currentDate.format("DD.MM.YYYY");
+  const formattedCurrentDate = currentDate.format("DD.MM.YYYY HH:mm");
   const currentDayOfWeek = moment().isoWeekday(); // Получаем текущий день недели (1 - понедельник, 2 - вторник, и так далее, 7 - воскресенье)
   const daysUntilSunday = 7 - currentDayOfWeek; // остаток дней до субботы
   const expiredDate = currentDate
     .clone()
     .add(1, "days")
     .set({ hour: 3, minute: 0, second: 0 })
-    .format("DD.MM.YYYY");
+    .format("DD.MM.YYYY HH:mm");
 
   console.log(`Current date - ${currentDate}`.yellow);
   console.log(`Formatted current date  - ${formattedCurrentDate}`.yellow);
