5773440b9d21f37378e59461d85bf6fa494491ca
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-09-28 23:08:13 +0300

    rework getter topics by language

--- src/server/utils/lessons/getTechProps/utils/getTopicsByLanguage.js
+++ src/server/utils/lessons/getTechProps/utils/getTopicsByLanguage.js
@@ -6,14 +6,14 @@ const TopicsList = require("../../../../models/Tech/TopicsList");
  *
  * @param {string} language - The language for which to retrieve topics.
  *
- * @returns {Promise<Array>} A promise that resolves to an array of topic objects.
+ * @returns {Promise<object>} A promise that resolves to an array of topic objects.
  *
  * @throws {Error} Throws an error if there is an issue fetching topics by language.
  */
 async function getTopicsByLanguage(language) {
   try {
-    const topicsList = await TopicsList.find({ language });
-    return topicsList;
+    const topicsList = await TopicsList.findOne({ language });
+    return topicsList.topics;
   } catch (e) {
     console.error("Error getting topics by language", e);
   }
