00ab97f97b3f35abeb52304e598f9facc1d37f46
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-01 00:14:47 +0300

    upd jsdocs

--- src/server/utils/lessons/getTechProps/getTechProps.js
+++ src/server/utils/lessons/getTechProps/getTechProps.js
@@ -4,12 +4,21 @@ const getTopicsChances = require("./utils/getTopicsChances");
 const getTopicsToChooseAmount = require("./utils/getTopicsToChooseAmount");
 
 /**
+ * Retrieves technical properties and configuration related to lessons and topics.
+ *
+ * @async
  * @function getTechProps
- * @description Retrieves technical properties and configuration related to lessons and topics.
- *
  * @param {string} language - The language for which to retrieve technical properties.
- *
- * @returns {Promise<Object>} A promise that resolves to an object containing the following properties:
+ * @returns {Promise<{
+ *   dayLesson: object,
+ *   weekLesson: object,
+ *   monthLesson: object,
+ *   topics: string[],
+ *   topicsToChoose: number,
+ *   chances: object,
+ *   lessonPoints: number,
+ *   missedLessonRadius: number
+ * }>} A promise that resolves to an object containing the following properties:
  *   - dayLesson (object): Configuration for daily lessons.
  *   - weekLesson (object): Configuration for weekly lessons.
  *   - monthLesson (object): Configuration for monthly lessons.
@@ -18,10 +27,9 @@ const getTopicsToChooseAmount = require("./utils/getTopicsToChooseAmount");
  *   - chances (object): Chances and formulas related to topic selection.
  *   - lessonPoints (number): Points associated with each lesson.
  *   - missedLessonRadius (number): The radius for missed lessons.
- *
  * @throws {Error} Throws an error if there is an issue fetching technical properties.
  */
-const getTechProps = async (language) => {
+async function getTechProps(language) {
   try {
     const [topicsToChooseAmount, topicsChances, topicsList, lessonConfig] =
       await Promise.all([
@@ -54,6 +62,6 @@ const getTechProps = async (language) => {
   } catch (e) {
     console.error("Error getting techProps", e);
   }
-};
+}
 
 module.exports = getTechProps;
