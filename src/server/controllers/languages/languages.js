155bba0401d666dfd58815968e5730355d88c09e
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-04 23:07:09 +0300

    move adding first topics and user.languages to add route

--- src/server/controllers/languages/languages.js
+++ src/server/controllers/languages/languages.js
@@ -19,48 +19,80 @@ async function get(req, res) {
   }
 }
 
-// This function adds a new language preference to the user.
+/**
+ * Add a new language to the user's profile.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Object} - JSON response containing the updated list of user languages and the added language object.
+ */
 async function add(req, res) {
   try {
-    // Retrieve the user's information based on their ID.
-    const user = await User.findById(req.user.id);
-
-    // Extract the new language from the request body.
+    // Extract user ID and new language from the request.
+    const userID = req.user.id;
     const newLanguage = req.body.language;
+
+    // Retrieve the user's information based on their ID, the available languages list, and topics list.
+    const [user, languagesList, topicsList] = await Promise.all([
+      User.findById(userID),
+      LanguagesList.findOne(),
+      TopicsList.findOne({ language: newLanguage }),
+    ]);
+
     // Check if the new language is provided.
-    if (!newLanguage)
+    if (!newLanguage) {
       return res.status(400).json({ message: "Language is required." });
+    }
 
-    // Add the new language to the user's languages array.
-    user.languages.push({ language: newLanguage, activeTopic: null });
-    // Set the language's points in the languagesPoints object.
     user.languagesPoints.set(newLanguage, 0);
-    // Save the changes to the user's information.
-    await user.save();
 
-    const languages = user.languages.map((language) => language);
+    // Find the language reference ID and name in the languages list.
+    const { _id: languageId, language: languageName } =
+      languagesList.languages.find((obj) => obj.language === newLanguage);
 
-    // Respond with the updated user's languages.
-    res.status(200).json(languages);
+    // Check if the active language object already exists in the user's profile.
+    const activeLanguageObject = user.languages.find((obj) => {
+      return obj.languageRef.toString() === languageId.toString();
+    });
+
+    if (!activeLanguageObject) {
+      // If not, create a new language object and add it to the user's profile.
+      const createdLanguageObject = {
+        languageRef: languageId,
+        activeTopicRef: [topicsList.topics[0]._id.toString()],
+        topicStatuses: [{ topicRef: topicsList.topics[0]._id, viewStatus: 1 }],
+      };
+      user.languages.push(createdLanguageObject);
+    }
+
+    user.save(); // without await to increase performance
+
+    // Retrieve the list of user languages.
+    const userLanguages = user.languages.map((language) => ({
+      _id: language.languageRef,
+      languageName: languagesList.languages.find(
+        (obj) => obj._id.toString() === language.languageRef.toString()
+      ).language,
+    }));
+
+    // Return the added language object and the updated list of user languages.
+    res
+      .status(201)
+      .json({
+        addedLanguage: { _id: languageId, languageName },
+        userLanguages,
+      });
   } catch (e) {
-    // Log the error and send a 500 Internal Server Error response.
     console.log(`Error while adding language to user object: ${e}`);
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: "Error adding new language." });
   }
 }
 
 // This function sets the active language for the user.
 async function setActive(req, res) {
   try {
-    const userID = req.user.id;
     const activeLanguage = req.body.language;
-
-    // Retrieve the user's information based on their ID.
-    const [user, languagesList, topicsList] = await Promise.all([
-      User.findById(userID),
-      LanguagesList.findOne(),
-      TopicsList.findOne({ language: activeLanguage }),
-    ]);
+    const user = await User.findById(req.user.id);
 
     // Check if the active language is provided.
     if (!activeLanguage) {
@@ -69,26 +101,9 @@ async function setActive(req, res) {
     // Set the user's active language.
     user.activeLanguage = activeLanguage;
 
-    const languageRef = languagesList.languages.find((obj) => {
-      return obj.language === activeLanguage;
-    })._id;
-
-    const activeLanguageObject = user.languages.find((obj) => {
-      return obj.languageRef.toString() === languageRef.toString();
-    });
-    if (!activeLanguageObject) {
-      // console.log("Adding language object to user.languages".yellow);
-      const createdLanguageObject = {
-        languageRef: languageRef,
-        activeTopicRef: [topicsList.topics[0]._id.toString()],
-        topicStatuses: [{ topicRef: topicsList.topics[0]._id, viewStatus: 1 }],
-      };
-      user.languages.push(createdLanguageObject);
-    }
-
     await user.save();
 
-    res.status(201).json(activeLanguage);
+    res.status(201).json({ activeLanguage });
   } catch (e) {
     console.log(`Error while setting active language for user: ${e}`);
     res.status(500).json({ message: e.message });
