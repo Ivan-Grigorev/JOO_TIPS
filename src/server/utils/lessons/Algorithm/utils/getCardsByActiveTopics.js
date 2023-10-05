b4855d999109f68307dda9740f7b01684fde4d88
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-05 17:59:55 +0300

    upd., add more comments and jsdocs annotations

--- src/server/utils/lessons/Algorithm/utils/getCardsByActiveTopics.js
+++ src/server/utils/lessons/Algorithm/utils/getCardsByActiveTopics.js
@@ -6,45 +6,45 @@ const Card = require("../../../../models/Card/Card");
  * @async
  * @function getCardsByActiveTopics
  * @param {string[]} activeTopics - An array of active topics.
+ * @param {string} language - The language for which to retrieve cards.
  * @param {string[]} allTakenCards - An array of card identifiers that the user has already taken.
- * @returns {Promise<{ cardIDs: {ref: string, topic: string,}[], findedCards: { topic: string, foundAmount: number, totalAmount: number }[] }>} An object containing card identifiers and information about the found cards.
+ * @returns {Promise<{ cardIDs: string[], findedCards: string[] }>} An object containing card identifiers and information about the found cards.
  * @throws {Error} An error if there was an issue fetching the data or processing the cards.
  */
 async function getCardsByActiveTopics(activeTopics, language, allTakenCards) {
   try {
-    const cardSet = new Set(); // Создаем сет для уникальных карточек
-    const findedCards = [];
+    const cardSet = new Set(); // Create a set for unique cards
+    const findedCards = new Set();
 
     for (const topic of activeTopics) {
       if (!topic) continue;
-      /* 
-       console.log(`topic - ${topic}`.blue);
-      console.log(`language - ${language}`.blue);
-      */
-      const findCards = await Card.find({ topic, language }); // Поиск карточек по заданной теме
+
+      const findCards = await Card.find({ topic, language }); // Search for cards based on the given topic and language
       findCards.forEach((card) => {
         const cardIdString = card._id.toString();
 
-        // Проверка на уникальность карточки
-        const isUniqueCard =!allTakenCards.includes(cardIdString) && !cardSet.has(cardIdString); // prettier-ignore
+        // Check if the card is unique
+        const isUniqueCard =
+          !allTakenCards.includes(cardIdString) && !cardSet.has(cardIdString);
 
         if (isUniqueCard) {
           cardSet.add(cardIdString);
-          findedCards.push({
-            ref: cardIdString,
-            topic: card.topic,
-          });
+          findedCards.add(cardIdString);
         }
       });
 
-      console.log(`Количество найденных карточек по теме "${topic}": ${findedCards.length}`); // prettier-ignore
+      console.log(
+        `Number of found cards for the topic "${topic}":`,
+        findedCards.size
+      );
     }
 
-    console.log("Общее количество найденных уникальных карточек:", findedCards.length); // prettier-ignore
+    console.log("Total number of found unique cards:", findedCards.size);
 
-    return { cardIDs: [...cardSet], findedCards };
+    return { cardIDs: [...cardSet], findedCards: [...findedCards] };
   } catch (e) {
     console.error("Error getting cards by user's active topics", e);
+    throw e; // Re-throw the error to indicate a problem with fetching or processing the cards
   }
 }
 
