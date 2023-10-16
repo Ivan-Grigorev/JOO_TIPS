145a9ee9c79319f8ed63983279dc2104f8a64f97
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-16 15:14:51 +0300

    upd., add comments and jsdocs

--- src/server/utils/lessons/getUserLanguagesInfo.js
+++ src/server/utils/lessons/getUserLanguagesInfo.js
@@ -1,32 +1,42 @@
 const LanguagesList = require("../../models/Tech/LanguagesList");
 const getTopicsByLanguage = require("./getTechProps/utils/getTopicsByLanguage");
 
+/**
+ * Retrieves information about a user's active languages and topics.
+ *
+ * @param {Object} user - The user object for which to fetch information.
+ * @returns {Promise<Object>} - A promise that resolves to an object containing
+ * the user's language object and an array of titles of their active topics.
+ */
 async function getUserLanguagesInfo(user) {
   try {
+    // Fetch the user's languages list and topics list concurrently
     const [languagesList, topicsList] = await Promise.all([
       LanguagesList.findOne({}),
       getTopicsByLanguage(user.activeLanguage),
     ]);
 
+    // Find the language reference based on the user's active language
     const languageRef = languagesList.languages.find((obj) => {
       return obj.language === user.activeLanguage;
     })._id;
 
+    // Find the user's language object based on the language reference
     const userLanguageObject = user.languages.find((obj) => {
       return obj.languageRef.toString() === languageRef.toString();
     });
 
+    // Get the references to the user's active topics
     const activeTopicRefs = userLanguageObject.activeTopicsRefs;
 
-    // Находим темы по их идентификаторам
+    // Retrieve the details of active topics by their references
     const activeTopics = activeTopicRefs.map((topicObject) => {
-      // Берём из объекта пользователя реф на тему
       return topicsList.find((topic) => {
-        // Ищем по рефу нужный нам объект в списке тем
         return topic._id.toString() === topicObject.ref.toString();
       });
     });
 
+    // Extract the titles of the active topics, handling potential null values
     const activeTopicsTitles = [
       activeTopics[0]?.topicTitle,
       activeTopics[1]?.topicTitle,
@@ -34,6 +44,7 @@ async function getUserLanguagesInfo(user) {
       activeTopics[3]?.topicTitle,
     ];
 
+    // Return the user's language object and active topics' titles
     return { userLanguageObject, activeTopicsTitles };
   } catch (e) {
     console.error(e);
