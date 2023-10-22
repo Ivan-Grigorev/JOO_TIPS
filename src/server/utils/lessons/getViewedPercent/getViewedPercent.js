c4d46ac8f15030068ea6e9563de48653fa6c3ebb
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-22 20:06:53 +0300

    upd., add status

--- src/server/utils/lessons/getViewedPercent/getViewedPercent.js
+++ src/server/utils/lessons/getViewedPercent/getViewedPercent.js
@@ -1,43 +1,34 @@
 const Card = require("../../../models/Card/Card");
-const getCardsCountByTopics = require("./utils/getCardsCountByTopic");
 
 /**
  * Middleware function to calculate the viewed percentage of cards for active topics.
  *
  * @param {object} userLanguageInfo - User's language information containing data for card views.
- * @returns {Promise<{ [topicTitle: string]: number }>} A Promise that resolves to an object where each key represents the title of an active topic (topicTitle) and the corresponding value is the count of viewed cards for that topic as a number. If no viewed cards are found for a topic, the count will be 0. This object represents the viewed card counts for each active topic.
+ * @returns {Promise<{ [topicTitle: string]: { cardsViewed: number, totalCards: number, viewedPercentage: number } }>} A Promise that resolves to an object where each key represents the title of an active topic (topicTitle) and the corresponding value is an object containing the count of viewed cards (cardsViewed), the total count of cards (totalCards), and the viewed percentage (viewedPercentage) as numbers. If no viewed cards are found for a topic, cardsViewed and viewedPercentage will be 0. This object represents the detailed card view information for each active topic.
  * The Promise resolves with this object once the calculations are complete.
  */
 async function getViewedPercent(userLanguageInfo) {
   try {
     const { userLanguageObject, activeTopicsTitles } = userLanguageInfo;
-
-    // Extract active topics references from userLanguageObject.
     const activeTopicsRefs = userLanguageObject.activeTopicsRefs;
-
-    // Extract topic statuses from userLanguageObject.
     const topicStatuses = userLanguageObject.topicStatuses;
 
-    // Create an array of topic refs as strings.
     const activeTopicsRefsArray = activeTopicsRefs.map((obj) =>
       obj.ref.toString()
     );
 
-    // Filter topic status objects based on active topic refs.
     const topicsStatusesObjects = topicStatuses.filter((obj) => {
       return activeTopicsRefsArray.includes(obj.topicRef.toString());
     });
 
     const viewedCardsByActiveTopic = {};
+
     for (let i = 0; i < activeTopicsRefsArray.length; i++) {
       const topicTitle = activeTopicsRefsArray[i];
-
-      // Find the corresponding active topic title from the provided titles array.
       const activeTopicTitle = activeTopicsTitles.find((topic) => {
         return topic.id === topicTitle;
       }).title;
 
-      // Count the total number of cards for the active topic.
       const totalCardsCount = await Card.countDocuments({
         topic: activeTopicTitle,
       });
@@ -47,7 +38,6 @@ async function getViewedPercent(userLanguageInfo) {
 
       let viewedCardsCount = 0;
 
-      // Determine the count of viewed cards based on the viewStatus.
       switch (viewStatus) {
         case 1:
           viewedCardsCount = cardViewStatuses.firstViewed.length;
@@ -63,16 +53,17 @@ async function getViewedPercent(userLanguageInfo) {
           break;
       }
 
-      viewedCardsByActiveTopic[topicTitle] = viewedCardsCount;
-
       const percentage = (viewedCardsCount / totalCardsCount) * 100;
-      console.log(
-        `Topic: ${topicTitle}, Cards viewed: ${viewedCardsCount}, Total cards: ${totalCardsCount}, Viewed percentage: ${percentage}%`
-          .yellow
-      );
+
+      viewedCardsByActiveTopic[topicTitle] = {
+        cardsViewed: viewedCardsCount,
+        totalCards: totalCardsCount,
+        viewedPercentage: percentage,
+        status: viewStatus,
+      };
     }
 
-    console.log("viewedCardsByActiveTopic".red, viewedCardsByActiveTopic);
+    // console.log("viewedCardsByActiveTopic".red, viewedCardsByActiveTopic);
 
     return viewedCardsByActiveTopic;
   } catch (e) {
