17652f7aed2da07ae577b1ab2982cbbfec439e69
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-22 19:32:21 +0300

    rework getViewedPercent function.

--- src/server/utils/lessons/getViewedPercent/getViewedPercent.js
+++ src/server/utils/lessons/getViewedPercent/getViewedPercent.js
@@ -4,32 +4,70 @@ const getCardsCountByTopics = require("./utils/getCardsCountByTopic");
 /**
  * @description Middleware to count viewed percent of the cards.
  *
- * @param {string} userObject - User object which data used to search Lessons linked with user.
+ * @param {object} userLanguageInfo - User object which data used to search Lessons linked with user.
  *
  * @returns {<Promise<number>>}
  */
 async function getViewedPercent(userLanguageInfo) {
   try {
-    const { activeTopicsTitles, userLanguageObject } = userLanguageInfo;
-    console.log("activeTopicsTitles".red, activeTopicsTitles);
+    const { userLanguageObject, activeTopicsTitles } = userLanguageInfo;
+    const activeTopicsRefs = userLanguageObject.activeTopicsRefs; // Извлекаем массив активных тем из объекта `userLanguageObject`
+    const topicStatuses = userLanguageObject.topicStatuses; // Извлекаем объект статусов тем из объекта `userLanguageObject`
 
-    // todo сделать цикл for для каждой из тем, чтобы узнать процент просмотра каждой темы
-    const totalCardsCountByActiveTopic = await Card.find({ topic: { $in: activeTopicsTitles }}).countDocuments(); // prettier-ignore
+    // Определяем массив рефов для активных тем
+    const activeTopicsRefsArray = activeTopicsRefs.map((obj) =>
+      obj.ref.toString()
+    );
+    // Определяем массив с объектами статусов тем
+    const topicsStatusesObjects = topicStatuses.filter((obj) => {
+      return activeTopicsRefsArray.includes(obj.topicRef.toString());
+    });
 
-    const viewedCardsByActiveTopic = getCardsCountByTopics(userLanguageObject) // prettier-ignore
+    const viewedCardsByActiveTopic = {};
 
-    console.log(`totalCardCount`.red, viewedCardsByActiveTopic);
-    // console.log(`User lessons amount`.red, totalCardsCountByActiveToppic);
+    for (let i = 0; i < activeTopicsRefsArray.length; i++) {
+      const topicTitle = activeTopicsRefsArray[i];
+      const activeTopicTitle = activeTopicsTitles.find((topic) => {
+        return topic.id === topicTitle;
+      }).title;
 
-    // todo проверка должна осуществляться учитывая индекс просмотренности тем в массиве .
-    // Рассчитайте процент привязанных карточек
-    const percentage =
-      (viewedCardsByActiveTopic.n / totalCardsCountByActiveTopic) * 100;
+      const totalCardsCount = await Card.countDocuments({
+        topic: activeTopicTitle,
+      });
 
-    return percentage;
+      const viewStatus = topicsStatusesObjects[i].viewStatus;
+      const cardViewStatuses = topicsStatusesObjects[i].cardViewStatus;
+
+      let viewedCardsCount = 0;
+
+      switch (viewStatus) {
+        case 1:
+          viewedCardsCount = cardViewStatuses.firstViewed.length;
+          break;
+        case 2:
+          viewedCardsCount = cardViewStatuses.secondViewed.length;
+          break;
+        case 3:
+          viewedCardsCount = cardViewStatuses.thirdViewed.length;
+          break;
+        default:
+          viewedCardsCount = 0;
+          break;
+      }
+
+      viewedCardsByActiveTopic[topicTitle] = viewedCardsCount;
+
+      const percentage = (viewedCardsCount / totalCardsCount) * 100;
+      console.log(
+        `Topic: ${topicTitle}, Cards viewed: ${viewedCardsCount}, Total cards: ${totalCardsCount}, Viewed percentage: ${percentage}%`
+          .yellow
+      );
+    }
+
+    return viewedCardsByActiveTopic;
   } catch (e) {
-    console.error("Error getting viewed cards percent");
-    throw new Error("Error getting viewed cards percent", e);
+    console.error("Error getting viewed cards percent", e);
+    throw Error("Error getting viewed cards percent", e);
   }
 }
 
