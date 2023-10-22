f5ba9e0ad0d65dbef899bb35261afb64fa125deb
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-22 16:56:01 +0300

    upd. comments

--- src/server/utils/lessons/getViewedPercent/utils/getCardsCountByTopic.js
+++ src/server/utils/lessons/getViewedPercent/utils/getCardsCountByTopic.js
@@ -1,26 +1,31 @@
 /**
  * Get the count of cards by topics.
- *
+ *Эта функция получает объект `languageObject`, представляющий информацию о языковых темах и статусах карт для этих тем.
  * @returns {{n: Number, n+1: Number ,n+2: Number, n+3: Number}} - An Object with counts for each topic.
  * @throws {Error} If there is an error while fetching the counts.
  */
+
 function getCardsCountByTopics(languageObject) {
   try {
-    const activeTopics = languageObject.activeTopicsRefs;
-    const topicStatuses = languageObject.topicStatuses;
+    const activeTopics = languageObject.activeTopicsRefs; // Извлекаем массив активных тем из объекта `languageObject`
+    const topicStatuses = languageObject.topicStatuses; // Извлекаем объект статусов тем из объекта `languageObject`
+
+    // Создаем пустой объект для хранения количества карт по темам
     const counts = {};
 
+    // Начинаем итерацию по активным темам
     for (let i = 0; i < activeTopics.length; i++) {
-      // Create an array of strings (refs) for active topics
+      // Создаем массив строк (refs) для активных тем
       const activeTopicsRefsArray = activeTopics.map((obj) => {
         return obj.ref.toString();
       });
 
-      // Filter topic objects based on refs
+      // Оставляем только статусы активных тем
       const topicsObjects = topicStatuses.filter((obj) => {
         return activeTopicsRefsArray.includes(obj.topicRef.toString());
       });
 
+      // Начинаем итерацию по активным темам
       for (let j = 0; j < topicsObjects.length; j++) {
         const topicObject = topicsObjects[j];
         const { viewStatus, cardViewStatus } = topicObject;
@@ -29,7 +34,7 @@ function getCardsCountByTopics(languageObject) {
 
         let count = null;
 
-        // Determine the count of cards based on viewStatus
+        // Определяем количество карт на основе статуса их просмотренности
         switch (viewStatus) {
           case 1:
             count = firstViewed.length;
@@ -48,13 +53,15 @@ function getCardsCountByTopics(languageObject) {
             break;
         }
 
+        // Генерируем номер темы в формате "n1", "n2", и т.д.
         const topicNumber = i === 0 ? "" : `+${i}`;
 
-        // Save the result in the counts object with keys n1, n2, etc.
+        // Сохраняем результат в объекте `counts` с ключами вида "n1", "n2", и т.д.
         counts[`n${topicNumber}`] = count;
       }
     }
 
+    // Возвращаем объект `counts` с количеством карт по темам
     return counts;
   } catch (e) {
     console.error("Error getting cards count by topics", e);
