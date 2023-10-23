e5ebe361e6aab07b9ca16756f79e5b78d96469e8
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-23 21:29:36 +0300

    add comments to getViewedPercent function

--- src/server/utils/lessons/getViewedPercent/getViewedPercent.js
+++ src/server/utils/lessons/getViewedPercent/getViewedPercent.js
@@ -4,22 +4,26 @@ const Card = require("../../../models/Card/Card");
  * Middleware function to calculate the viewed percentage of cards for active topics.
  *
  * @param {object} userLanguageInfo - User's language information containing data for card views.
- * @returns {Promise<{ [topicTitle: string]: { cardsViewed: number, totalCards: number, viewedPercentage: number } }>} A Promise that resolves to an object where each key represents the title of an active topic (topicTitle) and the corresponding value is an object containing the count of viewed cards (cardsViewed), the total count of cards (totalCards), and the viewed percentage (viewedPercentage) as numbers. If no viewed cards are found for a topic, cardsViewed and viewedPercentage will be 0. This object represents the detailed card view information for each active topic.
+ * @returns {Promise<{ [topicTitle: string]: { cardsViewed: number, totalCards: number, viewedPercentage: number, status: number } }>} A Promise that resolves to an object where each key represents the title of an active topic (topicTitle) and the corresponding value is an object containing the count of viewed cards (cardsViewed), the total count of cards (totalCards), the viewed percentage (viewedPercentage), and the status (viewStatus) as numbers. If no viewed cards are found for a topic, cardsViewed, viewedPercentage, and status will be 0. This object represents the detailed card view information for each active topic.
  * The Promise resolves with this object once the calculations are complete.
  */
 async function getViewedPercent(userLanguageInfo) {
   try {
+    // Extract relevant data from userLanguageInfo
     const { userLanguageObject, activeTopicsTitles } = userLanguageInfo;
     const activeTopicsRefs = userLanguageObject.activeTopicsRefs;
     const topicStatuses = userLanguageObject.topicStatuses;
 
+    // Convert activeTopicsRefs to an array of strings
     const activeTopicsRefsArray = activeTopicsRefs.map((obj) =>
       obj.ref.toString()
     );
 
+    // Filter topicsStatuses to get relevant objects
     const topicsStatusesObjects = topicStatuses.filter((obj) => {
       return activeTopicsRefsArray.includes(obj.ref.toString());
     });
+
     const viewedCardsByActiveTopic = {};
 
     for (let i = 0; i < activeTopicsRefsArray.length; i++) {
@@ -28,19 +32,18 @@ async function getViewedPercent(userLanguageInfo) {
         return topic.id === topicTitle;
       }).title;
 
+      // Get the total count of cards for the active topic
       const totalCardsCount = await Card.countDocuments({
         topic: activeTopicTitle,
       });
 
-      // console.log("topicsStatusesObjects[i]".green, topicsStatusesObjects[i]);
-
-      // console.log("topicsStatusesObjects[i]".green, topicsStatusesObjects[i]);
-
+      // Get viewStatus and cardViewStatus for the active topic
       const viewStatus = topicsStatusesObjects[i].viewStatus;
       const cardViewStatuses = topicsStatusesObjects[i].cardViewStatus;
 
       let viewedCardsCount = 0;
 
+      // Calculate the count of viewed cards based on viewStatus
       switch (viewStatus) {
         case 1:
           viewedCardsCount = cardViewStatuses.firstViewed.length;
@@ -56,8 +59,10 @@ async function getViewedPercent(userLanguageInfo) {
           break;
       }
 
+      // Calculate the percentage of viewed cards
       const percentage = (viewedCardsCount / totalCardsCount) * 100;
 
+      // Store the information in the viewedCardsByActiveTopic object
       viewedCardsByActiveTopic[topicTitle] = {
         cardsViewed: viewedCardsCount,
         totalCards: totalCardsCount,
@@ -66,8 +71,7 @@ async function getViewedPercent(userLanguageInfo) {
       };
     }
 
-    // console.log("viewedCardsByActiveTopic".red, viewedCardsByActiveTopic);
-
+    // Return the viewedCardsByActiveTopic object
     return viewedCardsByActiveTopic;
   } catch (e) {
     console.error("Error getting viewed cards percent", e);
