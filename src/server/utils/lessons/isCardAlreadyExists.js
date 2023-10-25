8f43ee5f6b536f3f82c122c82b6a4bab38d5af1b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-25 21:48:06 +0300

    write jsdocs for utils

--- src/server/utils/lessons/isCardAlreadyExists.js
+++ src/server/utils/lessons/isCardAlreadyExists.js
@@ -1,4 +1,17 @@
+/**
+ * Checks if a card with the given ID already exists in the view status array.
+ *
+ * @param {Array} viewStatusArray - The array representing the view status.
+ * @param {string} cardId - The ID of the card to check for.
+ * @returns {boolean} True if the card exists in the array, otherwise false.
+ */
 function isCardAlreadyExists(viewStatusArray, cardId) {
+  /**
+   * Check if a card with the specified ID exists in the view status array.
+   *
+   * @param {Object} obj - An object in the view status array.
+   * @returns {boolean} True if the card exists, otherwise false.
+   */
   const cardExists = viewStatusArray.some((obj) => {
     return obj.cardRef.toString() === cardId;
   });
--- src/server/utils/lessons/moveCardToNextArray.js
+++ src/server/utils/lessons/moveCardToNextArray.js
@@ -1,4 +1,15 @@
+/**
+ * Move a card to the next array in the card view status based on its position.
+ *
+ * @param {Object} cardViewStatus - The object representing card view status.
+ * @param {string} viewNumber - The current view number.
+ * @param {number} cardIndex - The index of the card in the current view.
+ * @param {string} cardId - The ID of the card to move.
+ */
 function moveCardToNextArray(cardViewStatus, viewNumber, cardIndex, cardId) {
+  /**
+   * Calculate the next view and the index of the card in the current view.
+   */
   const nextView = Object.keys(cardViewStatus)[cardIndex + 1];
   const existedCardIndex = cardViewStatus[viewNumber].findIndex(
     (obj) => obj.cardRef === cardId
@@ -10,4 +21,5 @@ function moveCardToNextArray(cardViewStatus, viewNumber, cardIndex, cardId) {
 
   console.log(`Moving the card to the ${nextView} array.`.yellow);
 }
+
 module.exports = moveCardToNextArray;
