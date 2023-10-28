0419714f9e1761dd5f147d4d0e60cedc950dd96c
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-10-28 22:15:18 +0300

    rework with a for loop, upd. jsdocs

--- src/server/utils/lessons/updatePercentage.js
+++ src/server/utils/lessons/updatePercentage.js
@@ -2,22 +2,26 @@
  * Module for updating the viewPercentage value in topicStatusObject.
  *
  * @param {object} userLanguageInfo - User language information containing card view data.
- * @param {object} topicStatusObject - An object representing the topic status to update the viewPercentage in.
- * @param {string} cardTopic - The identifier of the topic for which to update the viewPercentage.
+ * @param {object[]} topicStatusObject - An object representing the topic status to update the viewPercentage in.
+ * @param {string[]} cardTopics - The identifier of the topic for which to update the viewPercentage.
  * @returns {Promise<void>} A Promise that doesn't return a value. It updates the topicStatusObject.
  */
 const getViewedPercent = require("./getViewedPercent/getViewedPercent");
 
 async function updatePercentage(
   userLanguageInfo,
-  topicStatusObject,
-  cardTopic
+  topicsStatusesObjects,
+  cardsTopics
 ) {
   // Get information about card views
   const viewedPercent = await getViewedPercent(userLanguageInfo);
 
-  // Update the viewPercentage value in topicStatusObject using cardTopic
-  topicStatusObject.viewPercentage = viewedPercent[cardTopic].viewedPercentage; // prettier-ignore
+  for (let i = 0; i < cardsTopics.length; i++) {
+    const topicStatusObject = topicsStatusesObjects[i];
+
+    // Update the viewPercentage value in topicStatusObject using cardTopic
+    topicStatusObject.viewPercentage = viewedPercent[cardsTopics[i]].viewedPercentage; // prettier-ignore
+  }
 }
 
 module.exports = updatePercentage;
