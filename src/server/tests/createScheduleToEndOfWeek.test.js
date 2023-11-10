7ead88c188f5c49412879a1f957022f16762dcfc
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-11-11 01:02:24 +0200

    add comments

--- src/server/tests/createScheduleToEndOfWeek.test.js
+++ src/server/tests/createScheduleToEndOfWeek.test.js
@@ -3,24 +3,55 @@ const { mongoose } = require("mongoose");
 
 const mongoDB = require("../db.js");
 const { createScheduleToEndOfWeek } = require("../middlewares/lessons.js");
+const createLessons = require("../utils/lessons/createLessons.js");
+const getTechProps = require("../utils/lessons/getTechProps/getTechProps.js");
+const getCurrentDate = require("../utils/lessons/getCurrentDate.js");
 
 beforeAll(async () => await mongoDB());
 
-describe("createScheduleToEndOfWeek middleware (algorithm)", () => {
-  it("Should create the lessons for current week", async () => {
+describe("createScheduleToEndOfWeek middleware", () => {
+  const userId = "654d5389af1719bc76520019";
+  const language = "javascript";
+
+  // Получаем текущую дату и количество дней до воскресенья с помощью утилиты getCurrentDate
+  const { currentDate, daysUntilSunday } = getCurrentDate();
+
+  it("Should create the lessons for the current week", async () => {
+    // Создаем объект req, который бы обычно передавался в middleware
     const req = {
-      body: { language: "javascript" },
-      user: { id: "654d5389af1719bc76520019" },
+      body: { language },
+      user: { id: userId },
       scheduleIsExists: false,
     };
     const res = {};
     const next = jest.fn(); // Создаем mock функцию для next()
 
+    // Вызываем middleware для создания расписания
     await createScheduleToEndOfWeek(req, res, next);
 
-    // Проверяем, что next() была вызвана
+    // Проверяем, что функция next() была вызвана
     expect(next).toHaveBeenCalled();
+  });
+
+  it("Should create lessons amount depending on the date", async () => {
+    // Получаем технические свойства с использованием утилиты getTechProps
+    const techProps = await getTechProps(language);
+
+    // Создаем уроки для каждого дня до воскресенья
+    const lessonsToCreate = await createLessons.daily(
+      daysUntilSunday,
+      currentDate,
+      userId,
+      language,
+      techProps.dayLesson.cardsAmount,
+      techProps.dayLesson.cardsAmount * daysUntilSunday,
+      techProps.dayLesson.duration
+    );
+
+    // Проверяем, что создано правильное количество уроков
+    expect(lessonsToCreate).toHaveLength(daysUntilSunday);
   });
 });
 
+// Отключаемся от MongoDB после завершения всех тестов
 afterAll(async () => await mongoose.disconnect());
