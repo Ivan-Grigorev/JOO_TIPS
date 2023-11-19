3e85af0c6bb11b22b265ef72ef0a116d9fecd69a
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-11-19 17:11:58 +0200

    add while loop for a half year working

--- src/server/tests/Algorithm.test.js
+++ src/server/tests/Algorithm.test.js
@@ -1,34 +1,25 @@
 const request = require("supertest");
 const app = require("../app.js"); // Подключите ваше Express-приложение
 const moment = require("moment");
-const mongoose = require("mongoose");
-require("moment-timezone");
-const {
-  deleteCurrentUser,
-  getCurrentUser,
-} = require("../controllers/auth/auth.js");
-
 const mongoDB = require("../db.js");
-
-const getCurrentDate = require("../utils/lessons/getCurrentDate.js");
 const User = require("../models/user/user.js");
-
-beforeAll(async () => {
-  app.listen(80, async () => {
-    await mongoDB();
-  });
-});
-
-const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-const userData = {
-  name: "testUser-topics",
-  email: "testUser-topics@gmail.com",
-  password: "qwerty",
-  confirmedPassword: "qwerty",
-};
 
 describe("Test algorithm with 2 test users and logging topics", () => {
   var userToken;
+  let startDate;
+
+  const userData = {
+    name: "testUser-topics",
+    email: "testUser-topics@gmail.com",
+    password: "qwerty",
+    confirmedPassword: "qwerty",
+  };
+
+  beforeAll(async () => {
+    app.listen(80, async () => {
+      await mongoDB();
+    });
+  });
 
   it("Should create new test user", async () => {
     // Создаём тестового пользователя
@@ -58,17 +49,38 @@ describe("Test algorithm with 2 test users and logging topics", () => {
     ]);
 
     const user = await User.findOne({ email: userData.email });
-    const userHaveLanguageObject = user.languages.length === 1;
+    const userHaveLanguageObject = user.languages[0];
     const userHaveActiveLanguage = user.activeLanguage === "javascript";
 
-    expect(userHaveLanguageObject).toBe(true);
+    expect(userHaveLanguageObject).toBeDefined();
     expect(userHaveActiveLanguage).toBe(true);
   });
-});
 
-afterAll(async () => {
-  // Удаляем созданного тестового пользователя
-  await User.findOneAndDelete({ email: userData.email })
-    .then(console.log("Test user was deleted"))
-    .catch((e) => console.error(e));
+  it("Should create and finish lessons in half year with logging all used topics", async () => {
+    try {
+      const endDate = moment(startDate).add(6, "months"); // Добавляем полгода к начальной дате
+      let currentDate = moment(startDate);
+
+      while (currentDate.isSameOrBefore(endDate)) {
+        const formattedDate = currentDate.format("DD.MM.YYYY");
+        console.log("Текущая дата:", formattedDate);
+
+        // Выполнение тестов или действий с использованием formattedDate
+        console.info("Some action".blue);
+        // Увеличиваем текущую дату на один день
+        currentDate.add(1, "day");
+      }
+
+      console.log(`endDate, ${endDate}`.red);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  afterAll(async () => {
+    // Удаляем созданного тестового пользователя
+    await User.findOneAndDelete({ email: userData.email })
+      .then(console.log("Test user was deleted".green))
+      .catch((e) => console.error("Test user wasn't deleted".red));
+  });
 });
