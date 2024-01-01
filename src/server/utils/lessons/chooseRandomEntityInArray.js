267570bd60f5fd4ff50ca66a5643e7ba8237fefc
Vladislav Ulianeev <vladis.ulian@gmail.com>
2024-01-01 15:05:02 +0200

    upd. chooseRandomEntityInArray function. Add not neccessary argument - count of iteration

--- src/server/utils/lessons/chooseRandomEntityInArray.js
+++ src/server/utils/lessons/chooseRandomEntityInArray.js
@@ -1,9 +1,18 @@
 /**
  * Chooses a random entity from the given array.
  * @param {Array<string | Number | any>} array - The input array from which to choose a random entity.
- * @returns {string | Number | any} - A randomly selected entity from the array.
+ * @param {number} count - The number of random entities to select.
+ * @returns {Array<string | Number | any>} - An array of randomly selected entities from the input array.
  */
-function chooseRandomEntityInArray(array) {
-  return array[Math.floor(Math.random() * array.length)];
+function chooseRandomEntitiesInArray(array, count) {
+  const iterationCount = count || 1;
+  const randomEntities = [];
+
+  for (let i = 0; i < iterationCount; i++) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    randomEntities.push(array[randomIndex]);
+  }
+  return randomEntities;
 }
-module.exports = chooseRandomEntityInArray;
+
+module.exports = chooseRandomEntitiesInArray;
