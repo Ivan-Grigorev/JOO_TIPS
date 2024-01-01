0fbfce663de7191766af9d71ab6392565c519998
Vladislav Ulianeev <vladis.ulian@gmail.com>
2024-01-01 15:04:12 +0200

    add getLessonQuestions (displaying all lesson questions in array)

---
+++ src/server/utils/lessons/getLessonQuestions.js
@@ -0,0 +1,22 @@
+const getCardQuestions = require("./getCardQuestions");
+
+/**
+ * Retrieves questions for lesson cards based on their IDs.
+ * @param {Array<string>} lessonCardsIDs - Array of lesson card IDs.
+ * @returns {Promise<object>} - Promise resolving to an object containing card IDs as keys and arrays of questions as values.
+ */
+async function getQuestionsForLessonCards(lessonCardsIDs) {
+  const cardsQuestions = {};
+
+  for (const cardID of lessonCardsIDs) {
+    // Получаем вопросы для каждой карты по её ID
+    const cardQuestions = await getCardQuestions(cardID);
+
+    // Преобразуем вопросы в массив строк и сохраняем в объекте cardsQuestions
+    const cardArray = cardQuestions.map((question) => question.toString());
+    cardsQuestions[cardID] = cardArray;
+  }
+  return cardsQuestions; // Возвращаем объект с вопросами для каждой карты
+}
+
+module.exports = getQuestionsForLessonCards;
